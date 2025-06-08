8de83c27afcc822785ee834f393d6d197fbca60d
Jason <j-folkens@hotmail.com>
2025-06-08 08:51:07 -0400

    optimized ThreeSum

--- LeetCodeProblems/FirstAttempts/ThreeSum.cs
+++ LeetCodeProblems/FirstAttempts/ThreeSum.cs
@@ -8,11 +8,50 @@ namespace LeetCodeProblems.FirstAttempts
 {
     public class ThreeSumSolution
     {
+        // I had to review other's solutions before I really understood what was going on here.
+        // time complexity O(n^2) where n is the length of the array
+        // space complexity O(m) where m is the number lists to be returned
+
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            var returnable = new List<IList<int>>();
+
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                var left = i + 1;
+                var right = nums.Length - 1;
+                while (left < right)
+                {
+                    var sum = nums[left] + nums[right] + nums[i];
+                    if (sum < 0) left++;
+                    else if (sum > 0) right--;
+                    else
+                    {
+                        var item = new List<int>
+                        {
+                            nums[i],
+                            nums[left],
+                            nums[right]
+                        };
+
+                        returnable.Add(item);
+                        left++;
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                    }
+                }
+            }
+            return returnable;
+        }
+
+
         // this was my brute force attempt.  It appears to deliver the expected result, but the time complexity is unacceptable.
         // time complexity: O(n^3)
         // space complexity: O(n^3)
 
-        public IList<IList<int>> ThreeSum(int[] nums)
+        public IList<IList<int>> ThreeSumUnoptimized(int[] nums)
         {
             var returnable = new HashSet<(int, int, int)>();
             for (int i = 0; i < nums.Length; i++)
