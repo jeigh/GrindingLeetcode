c1dcb1d6d3bba6d52b9a9c54ed26fcf3b0dfa999
Jason <j-folkens@hotmail.com>
2025-06-16 09:26:23 -0400

    trapping rain water (optimized)

--- LeetCodeProblems/FirstAttempts/TrappingRainWater.cs
+++ LeetCodeProblems/FirstAttempts/TrappingRainWater.cs
@@ -59,5 +59,38 @@ namespace LeetCodeProblems.FirstAttempts
             suffixMaximums.Reverse();
             return suffixMaximums;
         }
+
+
+        // this is the optimized solution.  It individually calculates the water level per index instead of constructing a dedicated array for both prefix and suffix.
+        // time complexity: O(n), space complexity: O(1)
+
+        public int TrapOptimized(int[] height)
+        {
+            if (height == null || height.Length == 0) return 0;
+
+            int left = 0;
+            int right = height.Length - 1;
+
+            int leftMax = height[left];
+            int rightMax = height[right];
+
+            int returnable = 0;
+            while (left < right)
+            {
+                if (leftMax < rightMax)
+                {
+                    left++;
+                    leftMax = Math.Max(leftMax, height[left]);
+                    returnable += leftMax - height[left];
+                }
+                else
+                {
+                    right--;
+                    rightMax = Math.Max(rightMax, height[right]);
+                    returnable += rightMax - height[right];
+                }
+            }
+            return returnable;
+        }
     }
 }
