801bd037b110aebec3ac4707107250e1de7c835c
Jason <j-folkens@hotmail.com>
2025-06-27 09:02:42 -0400

    Add ReverseListSolution class for linked list reversal
    Introduces the `ReverseListSolution` class. The class
    includes a `ReverseList` method that reverses a singly
    linked list with O(n) time complexity and O(1) space
    complexity. Handles null input gracefully and uses a
    while loop to adjust node pointers until the end of the
    list is reached.

---
+++ LeetCodeProblems/FirstAttempts/ReverseListSolution.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.FirstAttempts
+{
+    public class ReverseListSolution
+    {
+        // time complexity: O(n)
+        // space complexity: O(1)
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null) return null;
+            ListNode currentNode = head;
+            ListNode prevNode = null;
+
+            while (true)
+            {
+                ListNode nextNode = currentNode.next;
+                currentNode.next = prevNode;
+
+                prevNode = currentNode;
+                if (nextNode != null) currentNode = nextNode;
+                else return currentNode;
+            }
+        }
+    }
+}
