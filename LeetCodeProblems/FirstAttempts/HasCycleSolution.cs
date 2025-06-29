0bfb9c75920309e6f8e67456904bc4895f0e6a57
Jason <j-folkens@hotmail.com>
2025-06-29 00:53:39 -0400

    Has Cycle Using Hash Set

---
+++ LeetCodeProblems/FirstAttempts/HasCycleSolution.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.FirstAttempts
+{
+    public class HasCycleSolution
+    {
+        // time complexity: O(n)
+        // space complexity: O(n)
+        public bool HasCycleUsingHashSet(ListNode head)
+        {
+            var hashSet = new HashSet<ListNode>();
+            ListNode current = head;
+            
+            while(true)
+            {
+                if (current == null) return false;
+                if (hashSet.Contains(current)) return true;
+                else hashSet.Add(current);
+                current = current.next;
+            }
+
+        }
+    }
+}
