b5548799babc4b2e809b0fd31dcea5c46458fd44
Jason <j-folkens@hotmail.com>
2025-06-30 08:52:29 -0400

    HasCycle - fast and slow pointers

--- LeetCodeProblems/FirstAttempts/HasCycleSolution.cs
+++ LeetCodeProblems/FirstAttempts/HasCycleSolution.cs
@@ -5,6 +5,27 @@ namespace LeetCodeProblems.FirstAttempts
     public class HasCycleSolution
     {
         // time complexity: O(n)
+        // space complexity: O(1)
+        public bool HasCycleUsingFastAndSlowPointers(ListNode head)
+        {
+            if (head == null) return false;
+            if (head?.next == null) return false;
+            
+            ListNode? fast = head;
+            ListNode? slow = head;
+
+            while (true)
+            {
+                fast = fast?.next?.next;
+                slow = slow?.next;
+
+                if (fast == null) return false;
+                if (fast == slow) return true;
+            }
+        }
+
+
+        // time complexity: O(n)
         // space complexity: O(n)
         public bool HasCycleUsingHashSet(ListNode head)
         {
@@ -18,7 +39,6 @@ namespace LeetCodeProblems.FirstAttempts
                 else hashSet.Add(current);
                 current = current.next;
             }
-
         }
     }
 }
