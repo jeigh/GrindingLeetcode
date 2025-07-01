4e53af4271d51327ed53cfa620a39e1020c0fb4a
Jason <j-folkens@hotmail.com>
2025-07-01 09:48:40 -0400

    moved stuff around;  renamed some of them

--- LeetCodeProblems/FirstAttempts/TopKFrequentSolution.cs
+++ LeetCodeProblems/FirstAttempts/TopKFrequentSolution.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace LeetCodeProblems.FirstAttempts
 {
 
-    public class TopKFrequentUnoptimizedSolution
+    public class TopKFrequentSolution
     {
         // this was my first attempt without looking at the solution
         // time complexity: O(n + u log u), which simplifies to O(n log n) in the worst case when all elements are unique.
@@ -32,5 +32,31 @@ namespace LeetCodeProblems.FirstAttempts
 
             return dict;
         }
+
+        // This is the solution provided by NeetCode
+        // Time Complexity: O(n + u log u + k), which simplifies to O(n log n) in the worst case when all elements are unique.
+        // Space Complexity: O(u + k), where u is the number of unique elements and k is the number of top frequent elements to return.
+
+        public int[] TopKFrequentOptimized(int[] nums, int k)
+        {
+            Dictionary<int, int> count = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (count.ContainsKey(num)) count[num]++;
+                else count[num] = 1;
+            }
+
+            List<int[]> arr = count.Select(entry => new int[] { entry.Value, entry.Key }).ToList();
+            arr.Sort((a, b) => b[0].CompareTo(a[0]));
+
+            int[] res = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                res[i] = arr[i][1];
+            }
+            return res;
+        }
     }
+
+
 }
--- LeetCodeProblems/FirstAttempts/TwoSumSolution.cs
+++ LeetCodeProblems/FirstAttempts/TwoSumSolution.cs
@@ -2,7 +2,7 @@
 
 namespace LeetCodeProblems.FirstAttempts
 {
-    public class TwoSumUnoptimizedSolution 
+    public class TwoSumSolution 
     {
         public int[] TwoSum(int[] nums, int target)
         {
@@ -19,6 +19,19 @@ namespace LeetCodeProblems.FirstAttempts
                 }
 
                 firstIndex += 1;
+            }
+            return [-1, -1];
+        }
+
+        public int[] TwoSumHashMap(int[] nums, int target)
+        {
+            Dictionary<int, int> addends = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (addends.ContainsKey(nums[i]))
+                    return [addends[nums[i]], i];
+
+                addends[target - nums[i]] = i;
             }
             return [-1, -1];
         }
