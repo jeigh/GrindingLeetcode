fe8277dee69a67462516caa5cdfe929f6eb79df6
Jason <j-folkens@hotmail.com>
2025-07-01 09:28:52 -0400

    Diameter of binary tree

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -8,22 +8,52 @@ namespace TwoSum
         static void Main(string[] args)
         {
 
-            var unit = new MergeTwoListsSolution();
-
-            int[] values1 = [1, 2, 4];
-            int[] values2 = [1, 3, 5];
-            var list1 = toLinkedList(values1);
-            var list2 = toLinkedList(values2);
-
-            var result = unit.MergeTwoLists(list1, list2);
+            var unit = new DiameterOfBinaryTreeSolution();
 
 
+            int?[] input = [4, -7, -3, null, null, -9, -3, 9, -7, -4, null, 6, null, -6, -6, null, null, 0, 6, 5, null, 9, null, null, -1, -4, null, null, null, -2];
+            TreeNode root = ToBinaryTree(input);
+
+            var result = unit.DiameterOfBinaryTree(root); 
 
             Console.WriteLine(result);
 
             Console.WriteLine("debugger");
 
         }
+
+        public static TreeNode ToBinaryTree(int?[] values)
+        {
+            if (values == null || values.Length == 0 || values[0] == null) return null;
+
+            TreeNode root = new TreeNode(values[0].Value);
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            int i = 1;
+
+            while (queue.Count > 0 && i < values.Length)
+            {
+                TreeNode current = queue.Dequeue();
+
+                // Left child
+                if (i < values.Length && values[i] != null)
+                {
+                    current.left = new TreeNode(values[i].Value);
+                    queue.Enqueue(current.left);
+                }
+                i++;
+
+                // Right child
+                if (i < values.Length && values[i] != null)
+                {
+                    current.right = new TreeNode(values[i].Value);
+                    queue.Enqueue(current.right);
+                }
+                i++;
+            }
+            return root;
+        }
+
 
         private static ListNode toLinkedList(int[] values)
         {
