df682b794625a6e4aa685ed854bdf2f3c3c0749f
Jason <j-folkens@hotmail.com>
2025-07-03 09:54:41 -0400

    is balanced tree

---
+++ LeetCodeProblems/IsBalancedTreeSolution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeProblems
+{
+    public class IsBalancedTreeSolution
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null) return true;
+            var depth = CheckDepth(root);
+            if (DepthDifference > 1) return false;
+            return true;
+        }
+
+        public int DepthDifference { get; set; }
+
+        public int CheckDepth(TreeNode node)
+        {
+            if (node == null) return 0;
+
+            var left = 1 + CheckDepth(node.left);
+            var right = 1 + CheckDepth(node.right);
+
+            if (DepthDifference > 1) return 0;
+            DepthDifference = Math.Max(Math.Abs(left - right), DepthDifference);
+            return Math.Max(left, right);
+        }
+    }
+}
