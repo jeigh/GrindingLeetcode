7474928f34ffd65e95dc3f348113ad04b707d6aa
Jason <j-folkens@hotmail.com>
2025-07-04 13:20:25 -0400

    Kth largest

---
+++ LeetCodeProblems/KthLargestUsingPriorityQueue.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeProblems
+{
+
+    public class KthLargestUsingPriorityQueue
+    {
+        // surprisingly, this solution actually runs slower than the List version on leetcode dispite having better time complexity.
+        // time complexity: O(n log k) space: O(log k) 
+        // where n is the number of elements.
+        private PriorityQueue<int, int> items = new PriorityQueue<int, int>();
+        private readonly int _k;
+
+        public KthLargestUsingPriorityQueue(int k, int[] nums)
+        {
+            this._k = k;
+            foreach (var item in nums)
+            {
+                Add(item);
+            }
+        }
+
+        public int Add(int val)
+        {
+            items.Enqueue(val, val);
+            if (items.Count > _k) items.Dequeue();
+            return items.Peek();
+        }
+    }
+}
