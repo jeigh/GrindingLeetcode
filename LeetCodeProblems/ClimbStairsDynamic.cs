622070334142dfe430cf2a4fa835d5edcc4ee8b8
Jason <j-folkens@hotmail.com>
2025-07-06 09:24:34 -0400

    Climb stairs dynamic

---
+++ LeetCodeProblems/ClimbStairsDynamic.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeProblems
+{
+    public class ClimbStairsDynamic
+    {
+        // time: O(n);  space: O(2)
+        public int ClimbStairs(int n)
+        {
+            int currentWays = 1;
+            int previousWays = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                var futurePreviousWays = currentWays;
+                previousWays = currentWays + previousWays;
+                previousWays = futurePreviousWays;
+            }
+
+            return currentWays;            
+        }
+    }
+}
