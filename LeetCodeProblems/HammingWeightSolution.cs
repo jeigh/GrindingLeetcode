2305c5ef2757bdfb2eea1048adceb3b9a4e44d08
Jason <j-folkens@hotmail.com>
2025-07-07 21:03:17 -0400

    hamming weight

---
+++ LeetCodeProblems/HammingWeightSolution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeProblems
+{
+    public class HammingWeightSolution
+    {
+        // time: O(1), space: O(1)
+        public int HammingWeight(uint n)
+        {
+            var result = 0;
+
+            while (n > 0)
+            {
+                if (n % 2 == 1) result++;
+                n >>= 1;                
+            }
+
+            return result;
+        }
+    }
+}
