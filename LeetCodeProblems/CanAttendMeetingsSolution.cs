314de50108f11f52c62478a44b171349d8e1a2bf
Jason <j-folkens@hotmail.com>
2025-07-08 09:33:13 -0400

    Can Attend Meetings

---
+++ LeetCodeProblems/CanAttendMeetingsSolution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeProblems
+{
+    public class CanAttendMeetingsSolution
+    {
+        public class Interval
+        {
+            public int start;
+            public int end;
+
+        }
+
+        // space complexity: O(1)  ; time complexity O(n log n)
+        public bool CanAttendMeetings(List<Interval> intervals)
+        {
+            intervals.Sort((a, b) => a.start.CompareTo(b.start));
+
+            for (int i = 0; i < intervals.Count - 1; i++)
+            {
+                if (intervals[i + 1].start - intervals[i].end < 0) return false;
+            }
+            return true;
+        }
+    }
+}
