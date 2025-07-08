87e5fcfa2bdba75ec053573fd0018ca76f8fba46
Jason <j-folkens@hotmail.com>
2025-07-08 09:33:33 -0400

    min cost climbing stairs

---
+++ LeetCodeProblems/MinCostClimbingStairsSolution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeProblems
+{
+    public class MinCostClimbingStairsSolution
+    {
+        // space complexity: O(n), Time Complexity O(1)
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            var twoPrev = cost[cost.Length - 1];
+            var onePrev = cost[cost.Length - 2];
+
+            for (int i = cost.Length - 3; i > -1; i--)
+            {
+                var curCost = Math.Min(cost[i] + twoPrev, cost[i] + onePrev);
+
+                var temp = onePrev;
+                onePrev = curCost;
+                twoPrev = temp;
+            }
+            return Math.Min(onePrev, twoPrev);
+        }
+    }
+}
