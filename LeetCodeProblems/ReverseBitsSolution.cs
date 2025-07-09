2acbc728ab9600bf628a03ff81b65ec36248bc95
Jason <j-folkens@hotmail.com>
2025-07-09 09:04:18 -0400

    reverse bits

---
+++ LeetCodeProblems/ReverseBitsSolution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeProblems
+{
+    public class ReverseBitsSolution
+    {
+        // space: O(1) time: O(1)
+        public uint ReverseBits(uint n)
+        {
+            uint result = 0;
+            
+            for(int i = 0; i < 32; i++)
+            {
+                result = (result << 1) | (1 & n);
+
+                n >>= 1;
+            }
+            return result;
+        }
+    }
+}
