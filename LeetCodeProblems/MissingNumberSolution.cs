ead45cf9f83167f6d9319d82d12eb30958ad64f8
Jason <j-folkens@hotmail.com>
2025-07-28 09:26:16 -0400

    missing number

---
+++ LeetCodeProblems/MissingNumberSolution.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeProblems
+{
+    public class MissingNumberSolution
+    {
+        // time: O(1), space: O(1)
+        public int MissingNumber(int[] nums)
+        {
+            var n = nums.Length;
+            int fullSum = n * (n + 1) / 2;
+            var partialSum = nums.Sum();
+            return fullSum - partialSum;
+        }
+    }
+}
