ed78bec4b0f5e0fd3d0244ab0f2eb7a54ccd5eb7
Jason <j-folkens@hotmail.com>
2025-09-15 09:08:08 -0400

    find min in rotated sorted array

---
+++ LeetCodeProblems/FindMinInRotatedSortedArraySolution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeProblems
+{
+    public class FindMinInRotatedSortedArraySolution
+    {
+        // time complexity O(log n)
+        public int FindMin(int[] nums)
+        {
+            int leftIndex = 0;
+            int rightIndex = nums.Length - 1;
+            while(leftIndex < rightIndex)
+            {
+                int currentIndex = leftIndex + (rightIndex - leftIndex) / 2;
+                if (nums[currentIndex] < nums[rightIndex]) rightIndex = currentIndex;
+                else leftIndex = currentIndex + 1;
+            }
+            return nums[leftIndex];
+        }
+    }
+}
