21b741ceef135ae82630a1ef231e7d567aaa19c2
Jason <j-folkens@hotmail.com>
2025-09-22 20:25:23 -0400

    SearchInRotatedSortedArray

---
+++ GrindingLeetCode.UnitTests/SearchInRotatedSortedArrayTests.cs
@@ -0,0 +1,129 @@
+﻿using LeetCodeProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GrindingLeetCode.UnitTests
+{
+    [TestClass]
+    public class SearchInRotatedSortedArrayTests
+    {
+        private SearchInRotatedSortedArraySolution _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new SearchInRotatedSortedArraySolution();
+        }
+
+        [TestMethod]
+        public void Search_NonRotatedArray_FindsElement()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            int target = 4;
+
+            // Act
+            int result = _solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Search_RotatedArray_FindsElementInLeftHalf()
+        {
+            // Arrange
+            int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+            int target = 5;
+
+            // Act
+            int result = _solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Search_RotatedArray_FindsElementInRightHalf()
+        {
+            // Arrange
+            int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+            int target = 0;
+
+            // Act
+            int result = _solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void Search_RotatedArray_ElementNotFound()
+        {
+            // Arrange
+            int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+            int target = 3;
+
+            // Act
+            int result = _solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void Search_SingleElementArray_ElementFound()
+        {
+            // Arrange
+            int[] nums = { 5 };
+            int target = 5;
+
+            // Act
+            int result = _solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Search_SingleElementArray_ElementNotFound()
+        {
+            // Arrange
+            int[] nums = { 5 };
+            int target = 7;
+
+            // Act
+            int result = _solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void Search_EmptyArray_ReturnsMinusOne()
+        {
+            // Arrange
+            int[] nums = { };
+            int target = 5;
+
+            // Act
+            int result = _solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void Search_FullyRotatedArray_FindsElement()
+        {
+            // Arrange - this is essentially a non-rotated array
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            int target = 6;
+
+            // Act
+            int result = _solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ LeetCodeProblems/SearchInrotatedSortedArraySolution.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCodeProblems
+{
+    public class SearchInRotatedSortedArraySolution
+    {
+        public int Search(int[] nums, int target)
+        {
+            var result = -1;
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                int midval = nums[mid];
+
+                if (midval == target) return mid;                
+
+                var SetLeftToNext =  () => left = mid + 1;
+                var SetRightToPrev = () => right = mid - 1;
+
+                int leftval = nums[left];
+                if (leftval <= midval)
+                {
+                    if (target > midval || target < leftval) 
+                        SetLeftToNext();
+
+                    else 
+                        SetRightToPrev();
+                }
+                else
+                {
+                    var rightVal = nums[right];
+                    if (target < midval || target > rightVal) 
+                        SetRightToPrev();
+
+                    else 
+                        SetLeftToNext();
+                }
+            }
+
+            return result;
+        }
+    }
+}
