aef42df498ed7c82e7eff1322a3273d1db41e9fd
Jason <j-folkens@hotmail.com>
2025-09-30 09:57:32 -0400

    I wanted to rewrite this so as to understand it better.

--- LeetCodeProblems/BinarySearch/SearchInRotatedSortedArraySolution_33.cs
+++ LeetCodeProblems/BinarySearch/SearchInRotatedSortedArraySolution_33.cs
@@ -1,45 +1,75 @@
-﻿namespace LeetCodeProblems.BinarySearch
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeetCodeProblems.BinarySearch
 {
     public class SearchInRotatedSortedArraySolution_33
     {
         public int Search(int[] nums, int target)
         {
-            var result = -1;
-
             int left = 0;
             int right = nums.Length - 1;
+            int mid = 0;
+            var moveLeftToNext = () => left = mid + 1;
+            var moveRightToPrev = () => right = mid - 1;
+
 
             while (left <= right)
             {
-                int mid = left + (right - left) / 2;
-                int midval = nums[mid];
+                mid = left + (right - left) / 2;
 
-                if (midval == target) return mid;                
+                if (nums[mid] == target) return mid;
 
-                var SetLeftToNext =  () => left = mid + 1;
-                var SetRightToPrev = () => right = mid - 1;
+                var midValueIsRightOfPivot = nums[mid] < nums[left];
+                var midValueIsLeftOfPivot = nums[mid] >= nums[left];
 
-                int leftval = nums[left];
-                if (leftval <= midval)
-                {
-                    if (target > midval || target < leftval) 
-                        SetLeftToNext();
+                var targetLTMidValue = target < nums[mid];
+                var targetGtMidValue = target > nums[mid];
+                var targetGteMidValue = target >= nums[mid];
 
-                    else 
-                        SetRightToPrev();
-                }
-                else
-                {
-                    var rightVal = nums[right];
-                    if (target < midval || target > rightVal) 
-                        SetRightToPrev();
+                var targetGteLeftValue = target >= nums[left];
+                var targetLtLeftValue = target < nums[left];
+                var targetEqLeftValue = target == nums[left];
 
-                    else 
-                        SetLeftToNext();
-                }
+                var targetGtRightValue = target > nums[right];
+                var targetLteRightValue = target <= nums[right];
+
+
+                
+
+                // Condition 1: Left is sorted, target is greater than midValue - search right
+                // we want a value between midvalue and pivot
+                if (midValueIsLeftOfPivot && targetGtMidValue)
+                    moveLeftToNext();
+
+                // Condition 2: Left is sorted, target is less than leftValue - search right
+                // we want a value to the right of pivot
+                if (midValueIsLeftOfPivot && targetLtLeftValue)
+                    moveLeftToNext();
+
+                // Condition 3: Left is sorted, target is between leftValue and midValue - search left
+                // we want a value between left and mid
+                if (midValueIsLeftOfPivot && targetGteLeftValue && targetLTMidValue)
+                    moveRightToPrev();
+
+                
+
+                // Condition 4: Right is sorted, target is less than midValue, target is less than leftValue - search left
+                // we want a value between pivot and midvalue
+                if (midValueIsRightOfPivot && targetLTMidValue && targetLtLeftValue)
+                    moveRightToPrev();
+
+                // Condition 6: Right is sorted, target is greater than rightValue - search left
+                // we want a value between pivot and midvalue
+                if (midValueIsRightOfPivot && targetGtRightValue)
+                    moveRightToPrev();
+
+                // Condition 7: Right is sorted, target is between midValue and rightValue - search right
+                // we want a value between midvalue and right
+                if (midValueIsRightOfPivot && targetGteMidValue && targetLteRightValue)
+                    moveLeftToNext();
             }
 
-            return result;
+            return -1;
         }
     }
 }
