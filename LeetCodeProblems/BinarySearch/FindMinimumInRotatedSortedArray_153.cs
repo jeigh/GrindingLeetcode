daf5f06cb10eddb39f4a9dcb5589ad5710adfcaa
Jason <j-folkens@hotmail.com>
2025-10-01 08:46:50 -0400

    find minimum in sorted array

---
+++ LeetCodeProblems/BinarySearch/FindMinimumInRotatedSortedArray_153.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeProblems.BinarySearch
+{
+    public class FindMinimumInRotatedSortedArray_153
+    {
+        public int FindMin(int[] nums)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            var mid = 0;
+            var minval = nums[0];
+            
+
+            while (left <= right)
+            {
+                mid = left + (right - left) / 2;
+
+                minval = Math.Min(nums[mid], minval);
+
+                var midIsLeftOfPivot = nums[0] <= nums[mid];
+                var midIsRightOfPivot = !midIsLeftOfPivot;
+
+                var midValIsLessThanRightVal = nums[mid] < nums[right];
+                
+
+                if (midIsLeftOfPivot) left = mid + 1;
+                if (midIsRightOfPivot) right = mid - 1;
+                
+            }
+
+            return minval;
+        }
+    }
+}
