6ac57af993f38f07d734da78c89e8507f92c973a
Jason <j-folkens@hotmail.com>
2025-10-05 21:51:29 -0400

    Split Array Largest Sum

---
+++ LeetCodeProblems/BinarySearch/SplitArrayLargestSumSolution_410.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeProblems.BinarySearch
+{
+    public class SplitArrayLargestSumSolution_410
+    {
+        public int SplitArray(int[] nums, int k)
+        {
+            if (k > nums.Count()) return -1;
+            var left = nums.Max();
+            var right = nums.Sum();
+            var mid = 0;
+            var returnable = 0;
+
+            while (left <=  right)
+            {
+                mid = left + (right - left) / 2;
+
+                if (CanSplit(nums, k, mid))
+                {
+                    returnable = mid;
+                    right = mid - 1;
+                }
+
+                else 
+                    left = mid + 1;
+
+            }
+            return returnable;
+        }
+
+        public bool CanSplit(int[] nums, int k, int maxSumPerSubArray)
+        {
+            if (k > nums.Count()) return false;
+
+            var subarrayCount = 1;
+            var currentSum = 0;
+            foreach( var x in nums)
+            {
+                if (x > maxSumPerSubArray) return false;
+
+                if (currentSum + x > maxSumPerSubArray) 
+                {
+                    currentSum = x;
+                    subarrayCount++;
+
+                    if (subarrayCount > k) return false;
+                }
+                else currentSum += x;
+            }
+
+            return true;
+        }
+    }
+}
