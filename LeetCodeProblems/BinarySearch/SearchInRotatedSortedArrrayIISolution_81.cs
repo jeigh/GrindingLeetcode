b4599e111c4360108fd1599c259e25f543a23870
Jason <j-folkens@hotmail.com>
2025-10-09 15:15:30 -0400

    Search in rotated sorted array II

---
+++ LeetCodeProblems/BinarySearch/SearchInRotatedSortedArrrayIISolution_81.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Security.Cryptography;
+
+namespace LeetCodeProblems.BinarySearch
+{
+    public class SearchInRotatedSortedArrrayIISolution_81
+    {
+        public bool Search(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0) return false;
+
+            int left = 0;
+            int right = nums.Length - 1;
+            var mid = 0;
+
+            var moveLeft = () => right = mid - 1;
+            var moveRight = () => left = mid + 1;
+
+            while (left <= right)
+            {
+                mid = left + (right - left) / 2;
+
+                if (nums[mid] == target || nums[left] == target || nums[right] == target)
+                    return true;
+
+                if (nums[left] == nums[mid])
+                {
+                    left++;
+                    continue;
+                }
+
+
+                bool isLeftSorted = nums[left] < nums[mid];
+
+                if (isLeftSorted)
+                {
+                    if (target > nums[left] && target < nums[mid])
+                        moveLeft();
+                    else
+                        moveRight();
+                }
+                else
+                {
+                    if (target > nums[mid] && target < nums[right])
+                        moveRight();
+                    else
+                        moveLeft();
+                }
+            }
+
+            return false;
+        }
+    }
+}
