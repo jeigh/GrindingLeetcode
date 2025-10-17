f22691a5650a7dc76ebb052e4dc7cd175bb1bf64
Jason <j-folkens@hotmail.com>
2025-10-17 10:19:51 -0400

    tests for 217

---
+++ GrindingLeetCode.UnitTests/ContainsDuplicateSolution_217_Tests.cs
@@ -0,0 +1,87 @@
+using LeetCodeProblems.HashingOrArrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GrindingLeetCode.UnitTests
+{
+    [TestClass]
+    public class ContainsDuplicateSolution_217_Tests
+    {
+        private ContainsDuplicateSolution_217 _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new ContainsDuplicateSolution_217();
+        }
+
+        [TestMethod]
+        public void HasDuplicateBruteForce_ContainsDuplicate_ReturnsTrue()
+        {
+            int[] nums = { 1, 2, 3, 1 };
+            Assert.IsTrue(_solution.hasDuplicate(nums));
+        }
+
+        [TestMethod]
+        public void HasDuplicateBruteForce_NoDuplicate_ReturnsFalse()
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            Assert.IsFalse(_solution.hasDuplicate(nums));
+        }
+
+        [TestMethod]
+        public void HasDuplicateBruteForce_EmptyArray_ReturnsFalse()
+        {
+            int[] nums = { };
+            Assert.IsFalse(_solution.hasDuplicate(nums));
+        }
+
+        [TestMethod]
+        public void HasDuplicateBruteForce_SingleElement_ReturnsFalse()
+        {
+            int[] nums = { 42 };
+            Assert.IsFalse(_solution.hasDuplicate(nums));
+        }
+
+        [TestMethod]
+        public void HasDuplicateBruteForce_AllDuplicates_ReturnsTrue()
+        {
+            int[] nums = { 7, 7, 7, 7 };
+            Assert.IsTrue(_solution.hasDuplicate(nums));
+        }
+
+        [TestMethod]
+        public void HasDuplicate_ContainsDuplicate_ReturnsTrue()
+        {
+            int[] nums = { 1, 2, 3, 1 };
+            Assert.IsTrue(_solution.hasDuplicate(nums));
+        }
+
+        [TestMethod]
+        public void HasDuplicate_NoDuplicate_ReturnsFalse()
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            Assert.IsFalse(_solution.hasDuplicate(nums));
+        }
+
+        [TestMethod]
+        public void HasDuplicate_EmptyArray_ReturnsFalse()
+        {
+            int[] nums = { };
+            Assert.IsFalse(_solution.hasDuplicate(nums));
+        }
+
+        [TestMethod]
+        public void HasDuplicate_SingleElement_ReturnsFalse()
+        {
+            int[] nums = { 42 };
+            Assert.IsFalse(_solution.hasDuplicate(nums));
+        }
+
+        [TestMethod]
+        public void HasDuplicate_AllDuplicates_ReturnsTrue()
+        {
+            int[] nums = { 7, 7, 7, 7 };
+            Assert.IsTrue(_solution.hasDuplicate(nums));
+        }
+    }
+}
