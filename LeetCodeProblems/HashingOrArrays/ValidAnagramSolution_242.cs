83c1574b555a1c903b414493bb37723dfb5e9800
Jason <j-folkens@hotmail.com>
2025-10-19 10:04:26 -0400

    valid anagram

---
+++ LeetCodeProblems/HashingOrArrays/ValidAnagramSolution_242.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeProblems.HashingOrArrays
+{
+    public class ValidAnagramSolution_242
+    {      
+        public bool IsAnagram(string s, string t)
+        {
+            int[] s_chars = NewMethod(s);
+            int[] t_chars = NewMethod(t);
+
+            for (int i = 0; i < s_chars.Length; i++)
+            {
+                if (s_chars[i] != t_chars[i]) return false;
+            }
+            return true;
+        }
+
+        private static int[] NewMethod(string str)
+        {
+            int[] str_chars = new int[26];
+            for (int i = 0; i < str.Length; i++)
+            {
+                var offset = str[i] - 'a';
+                str_chars[offset]++;
+            }
+
+            return str_chars;
+        }
+    }
+}
