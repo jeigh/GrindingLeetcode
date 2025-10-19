0ccce5d7b2b86a9a841c798013ab1854a0049d54
Jason <j-folkens@hotmail.com>
2025-10-19 10:04:39 -0400

    valid anagram tests

---
+++ GrindingLeetCode.UnitTests/ValidAnagramSolution_242_Tests.cs
@@ -0,0 +1,53 @@
+using LeetCodeProblems.HashingOrArrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GrindingLeetCode.UnitTests
+{
+    [TestClass]
+    public class ValidAnagramSolution_242_Tests
+    {
+        private ValidAnagramSolution_242 _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new ValidAnagramSolution_242();
+        }
+
+        [TestMethod]
+        public void IsAnagram_IdenticalStrings_ReturnsTrue()
+        {
+            Assert.IsTrue(_solution.IsAnagram("abc", "abc"));
+        }
+
+        [TestMethod]
+        public void IsAnagram_AnagramStrings_ReturnsTrue()
+        {
+            Assert.IsTrue(_solution.IsAnagram("anagram", "nagaram"));
+        }
+
+        [TestMethod]
+        public void IsAnagram_DifferentLengthStrings_ReturnsFalse()
+        {
+            Assert.IsFalse(_solution.IsAnagram("rat", "carp"));
+        }
+
+        [TestMethod]
+        public void IsAnagram_NotAnagram_ReturnsFalse()
+        {
+            Assert.IsFalse(_solution.IsAnagram("hello", "bello"));
+        }
+
+        [TestMethod]
+        public void IsAnagram_EmptyStrings_ReturnsTrue()
+        {
+            Assert.IsTrue(_solution.IsAnagram("", ""));
+        }
+
+        [TestMethod]
+        public void IsAnagram_OneEmptyOneNonEmpty_ReturnsFalse()
+        {
+            Assert.IsFalse(_solution.IsAnagram("", "a"));
+        }
+    }
+}
