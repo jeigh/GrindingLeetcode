c4fdcfa47bb4c188aee4470925f1cd3be4f8928c
Jason <j-folkens@hotmail.com>
2025-10-20 09:24:29 -0400

    remove element solution

---
+++ LeetCodeProblems/HashingOrArrays/RemoveElementSolution_27.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeProblems.HashingOrArrays
+{
+    public class RemoveElementSolution_27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            var k = 0;
+
+            for ( var i = 0; i < nums.Length; i++ ) 
+            {
+                if (nums[i] != val)
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+}
