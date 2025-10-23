926772aaeffef859a12579d81f86063de879d1f0
Jason <j-folkens@hotmail.com>
2025-10-23 09:00:59 -0400

    concatenation of array

---
+++ LeetCodeProblems/HashingOrArrays/ConcatenationOfArraySolution_1929.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeetCodeProblems.Hashing
+{
+    public class ConcatenationOfArraySolution_1929
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            var returnable = new List<int>(nums.Count() * 2);
+
+            foreach (var item in nums)
+            {
+                returnable.Add(item);
+            }
+            foreach (var item in nums)
+            {
+                returnable.Add(item);
+            }
+            return returnable.ToArray();
+        }
+    }
+}
