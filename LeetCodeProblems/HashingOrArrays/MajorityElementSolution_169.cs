8f07e8df68e37f5decfd13b0a5d6a20fe226f5b2
Jason <j-folkens@hotmail.com>
2025-10-27 08:48:17 -0400

    majority element solution

---
+++ LeetCodeProblems/HashingOrArrays/MajorityElementSolution_169.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeProblems.HashingOrArrays
+{
+    public class MajorityElementSolution_169
+    {
+        // neetcode calls this the boyer-moore voting algorithm
+        public int MajorityElement(int[] nums)
+        {
+            int returnable = nums[0];
+            var returnableCount = 0;
+
+            foreach(int x in nums)
+            {
+                if (returnable == x)
+                {
+                    returnableCount++;
+                }
+                else
+                {
+                    if (returnableCount > 0) returnableCount--;
+                    else 
+                    { 
+                        returnable = x; 
+                        returnableCount++; 
+                    }                   
+                }
+            }
+
+            return returnable;
+        }
+    }
+}
