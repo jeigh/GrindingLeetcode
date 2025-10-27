50b45743fb99c382d6384a2305b585f5dacc1783
Jason <j-folkens@hotmail.com>
2025-10-27 08:47:54 -0400

    remove duplicates from sorted array hashset solution

---
+++ LeetCodeProblems/HashingOrArrays/RemoveDuplicatesFromSortedArrayHashsetSolution_26.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeProblems.HashingOrArrays
+{
+
+    public class RemoveDuplicatesFromSortedArrayHashsetSolution_26
+    {
+        // this might not be optimized, but it's the solution I came up with organically that uses a hashset instead of twopointers.
+        // time complexity: O(n)
+        // space complexity: O(n)
+
+        public int RemoveDuplicates(int[] nums)
+        {
+            var hashset = new HashSet<int>();
+            int[] originalNums = new int[nums.Length];
+            Array.Copy(nums, originalNums, nums.Length);
+            var insertionPoint = 0;
+
+            foreach (int x in nums)
+            {
+                if (!hashset.Contains(x))
+                {
+                    hashset.Add(x);
+                    nums[insertionPoint] = x;
+                    insertionPoint++;
+                }
+            }
+            return hashset.Count;
+
+        }
+    }
+}
