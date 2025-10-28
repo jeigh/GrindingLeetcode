b95aabaf67c4e83202580f05d464f1a4bba463b1
Jason <j-folkens@hotmail.com>
2025-10-28 12:18:49 -0400

    TwoSum Hashmap solution

---
+++ LeetCodeProblems/HashingOrArrays/TwoSumHashMapSolution_1.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.HashingOrArrays
+{
+    public class TwoSumHashMapSolution_1 : ITwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> neededValueToIndex = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (neededValueToIndex.ContainsKey(nums[i]))
+                    return [neededValueToIndex[nums[i]], i];
+
+                neededValueToIndex[target - nums[i]] = i;
+            }
+            return [-1, -1];
+        }
+    }
+}
