0af66ada5c13dfe253dba076af05fcf134954eb2
Jason <j-folkens@hotmail.com>
2025-10-29 08:54:22 -0400

    Two sum two pointers solution

---
+++ LeetCodeProblems/HashingOrArrays/TwoSumTwoPointersSolution_1.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeProblems.HashingOrArrays
+{
+    public class TwoSumTwoPointersSolution_1 : ITwoSum 
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int firstIndex = 0;
+            foreach (int firstAddend in nums)
+            {
+                int secondIndex = 0;
+                foreach (int secondAddend in nums)
+                {
+                    if (firstIndex == secondIndex) continue;
+                    if (firstAddend + secondAddend == target) return [secondIndex, firstIndex];
+
+                    secondIndex += 1;
+                }
+
+                firstIndex += 1;
+            }
+            return [-1, -1];
+        }
+    }
+}
