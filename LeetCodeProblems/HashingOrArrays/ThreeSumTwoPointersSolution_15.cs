3d15aa02f220e74733fa30dd617f97f7450c0771
Jason <j-folkens@hotmail.com>
2025-11-01 15:45:48 -0400

    fixing build

---
+++ LeetCodeProblems/HashingOrArrays/ThreeSumTwoPointersSolution_15.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeProblems.HashingOrArrays
+{
+
+
+    /// <summary>
+    /// Given an integer array nums, return all the triplets[nums[i], nums[j], nums[k]] 
+    /// such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+    /// Notice that the solution set must not contain duplicate triplets.
+    /// </summary>
+    public class ThreeSumTwoPointersSolution_15
+    {
+        public List<List<int>> ThreeSum(int[] nums)
+        {
+            return null;
+        }
+    }
+}
