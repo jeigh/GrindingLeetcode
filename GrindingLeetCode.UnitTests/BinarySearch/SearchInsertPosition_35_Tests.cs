e23897777032e79cc54851831b9b71c50a57f40d
Jason <j-folkens@hotmail.com>
2025-11-01 15:32:43 -0400

    moved tests for 35

--- GrindingLeetCode.UnitTests/BinarySearch/SearchInsertPosition_35_Tests.cs
+++ GrindingLeetCode.UnitTests/BinarySearch/SearchInsertPosition_35_Tests.cs
@@ -1,7 +1,7 @@
 using LeetCodeProblems.BinarySearch;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace GrindingLeetCode.UnitTests
+namespace GrindingLeetCode.UnitTests.BinarySearch
 {
     [TestClass]
     public class SearchInsertPosition_35_Tests
