f61810264270307c41257bb87bc59a9d1cad5e1c
Jason <j-folkens@hotmail.com>
2025-11-02 08:25:10 -0500

    moving tests for lc 1

--- GrindingLeetCode.UnitTests/TwoPointers/TwoSumTwoPointerSolution_1_Tests.cs
+++ GrindingLeetCode.UnitTests/TwoPointers/TwoSumTwoPointerSolution_1_Tests.cs
@@ -1,6 +1,7 @@
 using LeetCodeProblems.HashingOrArrays;
+using LeetCodeProblems.Shared;
 
-namespace GrindingLeetCode.UnitTests
+namespace GrindingLeetCode.UnitTests.TwoPointers
 {
     [TestClass]
     public class TwoSumTwoPointerSolution_1_Tests
