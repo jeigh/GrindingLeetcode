431cb49142065919385f113e703e0d7fec8006f3
Jason <j-folkens@hotmail.com>
2025-11-03 09:28:18 -0500

    fixing the build

---
+++ LeetCodeProblems/ILeetCode424.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeProblems
+{
+    /// <summary>
+    /// You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+    ///
+    /// Return the length of the longest substring containing the same letter you can get after performing the above operations.
+    /// </summary>
+    public interface ILeetCode424
+    {
+
+        public int CharacterReplacement(string s, int k);
+    }
+}
