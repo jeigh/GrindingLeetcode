5a442a2a9ecbce35958ccfb80da28756f27ea693
Jason <j-folkens@hotmail.com>
2025-11-03 09:27:26 -0500

    moving 424

--- LeetCodeProblems/BruteForce/LongestRepeatingCharacterReplacementBruteForceSolution_424.cs
+++ LeetCodeProblems/BruteForce/LongestRepeatingCharacterReplacementBruteForceSolution_424.cs
@@ -2,14 +2,16 @@
 using System.Reflection.Metadata.Ecma335;
 using System.Security.AccessControl;
 
-namespace LeetCodeProblems
+namespace LeetCodeProblems.Bespoke
 {
+
+
     // this is the brute force solution that I came up with after understanding the problem
     // i'm pretty sure this does what's expected, but runs too slowly when ran on large inputs.
     // time complexity: O(n^3) (because of the triple-nested loops),  space complexity: O(n)
-    public class LongestRepeatingCharacterReplacementSolution
+    public class LongestRepeatingCharacterReplacementBruteForceSolution_424 : ILeetCode424
     {
-        public int CharacterReplacementBruteForce(string s, int k)
+        public int CharacterReplacement(string s, int k)
         {
             int returnable = 0;
             int left = 0;
@@ -57,50 +59,6 @@ namespace LeetCodeProblems
 
         }
 
-        private void RemoveOrDecrementFromHashMap(Dictionary<char, int> charCounts, char c)
-        {
-            //if (!charCounts.ContainsKey(c)) throw new ArgumentOutOfRangeException(nameof(c));
-            if (charCounts.ContainsKey(c) && charCounts[c] == 1) charCounts.Remove(c);
-            else if (charCounts.ContainsKey(c)) charCounts[c]--;
-        }
 
-
-
-        // i had to go review someone else's solution to come up with this
-        // time complexity: O(n*m) where n is the length of s, and m is the number of unique characters.
-        // space complexity: O(m) because of the hashmap
-        public int CharacterReplacementSlidingWindow(string s, int k)
-        {
-            var hashmap = new Dictionary<char, int>();
-            int returnable = 0;
-            int left = 0;
-            int right = 0;
-            var maximumHashmapValue = 0;
-            while (right < s.Length)
-            {
-                char rightChar = s[right];
-                char leftChar = s[left];
-                AddOrIncrementToHashMap(hashmap, rightChar);
-
-                int windowSize = right - left + 1;
-                
-                maximumHashmapValue = Math.Max(maximumHashmapValue, hashmap[rightChar]);
-
-                while (windowSize - maximumHashmapValue > k)
-                {
-                    RemoveOrDecrementFromHashMap(hashmap, leftChar);
-
-                    left++;
-                    leftChar = s[left];
-                    windowSize = right - left + 1;
-                }
-
-                returnable = Math.Max(returnable, windowSize);
-                var currentSubstring = s.Substring(left, windowSize);
-                right++;
-            }
-
-            return returnable;
-        }
     }
 }
---
+++ LeetCodeProblems/SlidingWindow/LongestRepeatingCharacterReplacementSlidingWindowSolution_424.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCodeProblems.SlidingWindow
+{
+    public class LongestRepeatingCharacterReplacementSlidingWindowSolution_424 : ILeetCode424
+    {
+        // i had to go review someone else's solution to come up with this
+        // time complexity: O(n*m) where n is the length of s, and m is the number of unique characters.
+        // space complexity: O(m) because of the hashmap
+        public int CharacterReplacement(string s, int k)
+        {
+            var hashmap = new Dictionary<char, int>();
+            int returnable = 0;
+            int left = 0;
+            int right = 0;
+            var maximumHashmapValue = 0;
+            while (right < s.Length)
+            {
+                char rightChar = s[right];
+                char leftChar = s[left];
+                AddOrIncrementToHashMap(hashmap, rightChar);
+
+                int windowSize = right - left + 1;
+
+                maximumHashmapValue = Math.Max(maximumHashmapValue, hashmap[rightChar]);
+
+                while (windowSize - maximumHashmapValue > k)
+                {
+                    RemoveOrDecrementFromHashMap(hashmap, leftChar);
+
+                    left++;
+                    leftChar = s[left];
+                    windowSize = right - left + 1;
+                }
+
+                returnable = Math.Max(returnable, windowSize);
+                var currentSubstring = s.Substring(left, windowSize);
+                right++;
+            }
+
+            return returnable;
+        }
+
+        private void RemoveOrDecrementFromHashMap(Dictionary<char, int> charCounts, char c)
+        {
+            //if (!charCounts.ContainsKey(c)) throw new ArgumentOutOfRangeException(nameof(c));
+            if (charCounts.ContainsKey(c) && charCounts[c] == 1) charCounts.Remove(c);
+            else if (charCounts.ContainsKey(c)) charCounts[c]--;
+        }
+
+        private void AddOrIncrementToHashMap(Dictionary<char, int> charCounts, char c)
+        {
+            if (charCounts.ContainsKey(c)) charCounts[c]++;
+            else charCounts.Add(c, 1);
+
+        }
+    }
+}
