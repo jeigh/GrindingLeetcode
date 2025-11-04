0ba4296fd6e1dd3a9e2dc711586e35b7daeff0ee
Jason <j-folkens@hotmail.com>
2025-11-04 11:44:04 -0500

    added a vb file

---
+++ LeetCodeProblems.VisualBasic/Unfinished/ContainsDuplicateIISolution_219.vb
@@ -0,0 +1,9 @@
+Namespace Unfinished
+    Public Class ContainsDuplicateIISolution_219
+        Public Function ContainsNearbyDuplicate(nums As Integer(), k As Integer) As Boolean
+
+        End Function
+    End Class
+
+End Namespace
+
