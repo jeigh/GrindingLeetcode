6fb1422eeb449602142ec3ba99d91fdfb9a30eed
Jason <j-folkens@hotmail.com>
2025-11-06 09:38:11 -0500

    added 680

---
+++ LeetCodeProblems/TwoPointers/ValidPalindromeIISolution_680.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeProblems.TwoPointers
+{
+    public class ValidPalindromeIISolution_680
+    {
+        // space complexity:  O(1)
+        // time complexity:  O(n)
+        public bool ValidPalindrome(string s)
+        {
+            var left = 0;
+            var right = s.Length - 1;
+            while (left < right)
+            {
+                if (s[left] == s[right])
+                {
+                    left++;
+                    right--;
+                }
+                else if (IsPalindrome(s, left, right - 1)) return true;
+                else if (IsPalindrome(s, left + 1, right)) return true;
+                else return false;
+            }
+            return true;
+        }
+
+        public bool IsPalindrome(string s, int left, int right)
+        {
+            while (left < right)
+            {
+                if (s[left] == s[right])
+                {
+                    right--;
+                    left++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
