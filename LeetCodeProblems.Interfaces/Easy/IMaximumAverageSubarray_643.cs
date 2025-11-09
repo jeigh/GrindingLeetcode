728b332a71aa260e8cfc5af24d1a414f6a052132
Jason <j-folkens@hotmail.com>
2025-11-09 09:04:29 -0500

    added interface for 643

---
+++ LeetCodeProblems.Interfaces/Easy/IMaximumAverageSubarray_643.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// You are given an integer array nums consisting of n elements, and an integer k.
+    ///
+    /// Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.Any answer with a calculation error less than 10^(-5) will be accepted.
+    /// </summary>
+    public interface IMaximumAverageSubarray_643
+    {
+        public double FindMaxAverage(int[] nums, int k);
+    }
+
+
+}
