17dab95a1ba5c38ada57f36d9f70aa108360dbea
Jason <j-folkens@hotmail.com>
2025-11-10 09:43:00 -0500

    added interface for leetcode 209

---
+++ LeetCodeProblems.Interfaces/Medium/IMinimumSizeSubarraySum_209.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeProblems.Interfaces.Medium
+{
+    /// <summary>
+    /// Given an array of positive integers nums and a positive integer target, 
+    /// return the minimal length of a subarray whose sum is greater than or equal to target. 
+    /// If there is no such subarray, return 0 instead.
+    /// </summary>
+    public interface IMinimumSizeSubarraySum_209
+    {
+        public int MinSubArrayLen(int target, int[] nums);
+    }
+}
