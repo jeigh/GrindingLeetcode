15c7b0d08c11f61ddd0e00c40390d147c61912a7
Jason <j-folkens@hotmail.com>
2025-11-14 08:58:05 -0500

    added reverse string (344)

---
+++ LeetCodeProblems/TwoPointers/ReverseString_344.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeProblems.TwoPointers
+{
+    public class ReverseString_344
+    {
+        public void ReverseString(char[] s)
+        {
+            if (s == null || s.Length == 0) return;
+            var left = 0;
+            var right = s.Length - 1;
+
+            while (left<right)
+            {
+                swap(s, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        public void swap(char[] s, int left, int right)
+        {
+            var temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+        }
+    }
+}
