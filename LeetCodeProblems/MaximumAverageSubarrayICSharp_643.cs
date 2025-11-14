90526a8581bbe329ff2ade065c0e3a38483e3dcb
Jason <j-folkens@hotmail.com>
2025-11-14 08:54:49 -0500

    added maximum Average Subarray

---
+++ LeetCodeProblems/MaximumAverageSubarrayICSharp_643.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+
+namespace LeetCodeProblems
+{
+    public class MaximumAverageSubarrayICSharp_643 : IMaximumAverageSubarray_643
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            int currentSum = 0;
+
+            for (int i = 0; i < k; i++)
+                currentSum += nums[i];
+
+            int maxSum = currentSum;
+
+            for (int right = k; right < nums.Length; right++)
+            {
+                int left = right - k;
+                currentSum += nums[right] - nums[left];  
+                maxSum = Math.Max(currentSum, maxSum);
+            }
+
+            return maxSum / (double)k;
+        }
+    }
+}
