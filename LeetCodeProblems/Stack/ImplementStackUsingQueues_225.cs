a191d737a8b80a145c284c8dccd278f73dc26065
Jason <j-folkens@hotmail.com>
2025-11-15 10:42:36 -0500

    added ImplementStackUsingQueues

---
+++ LeetCodeProblems/Stack/ImplementStackUsingQueues_225.cs
@@ -0,0 +1,34 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using System.Collections;
+
+namespace LeetCodeProblems
+{
+    public class ImplementStackUsingQueues_225 : IImplementStackUsingQueues_225
+    {
+        private PriorityQueue<int, int> myQueue = new();
+
+        public ImplementStackUsingQueues_225()
+        {
+        }
+
+        public bool Empty()
+        {
+            return myQueue.Count == 0;
+        }
+
+        public int Pop()
+        {
+            return myQueue.Dequeue();
+        }
+
+        public void Push(int x)
+        {
+            myQueue.Enqueue(x, -myQueue.Count);
+        }
+
+        public int Top()
+        {
+            return myQueue.Peek();
+        }
+    }
+}
