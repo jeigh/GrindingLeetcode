d405bb66f5c43d0f8826f372a0214307392d82eb
Jason <j-folkens@hotmail.com>
2025-11-15 10:38:13 -0500

    added vb version of 150

---
+++ LeetCodeProblems.VisualBasic/Stack/EvaluateReversePolishNotationVB_150.vb
@@ -0,0 +1,44 @@
+﻿Imports LeetCodeProblems.Interfaces.Medium
+
+Namespace Stack
+
+
+    Public Class EvaluateReversePolishNotationVB_150
+        Implements IEvaluateReversePolishNotationCSharp_150
+
+        Private _stack As New Stack(Of Integer)
+
+
+        Public Function EvalRPN(tokens() As String) As Integer Implements IEvaluateReversePolishNotationCSharp_150.EvalRPN
+            For Each token In tokens
+                Select Case token
+                    Case "+"
+                        Dim first = _stack.Pop()
+                        Dim second = _stack.Pop()
+                        Dim pushable = second + first
+                        _stack.Push(pushable)
+                    Case "-"
+                        Dim first = _stack.Pop()
+                        Dim second = _stack.Pop()
+                        Dim pushable = second - first
+                        _stack.Push(pushable)
+                    Case "*"
+                        Dim first = _stack.Pop()
+                        Dim second = _stack.Pop()
+                        Dim pushable = second * first
+                        _stack.Push(pushable)
+                    Case "/"
+                        Dim first = _stack.Pop()
+                        Dim second = _stack.Pop()
+                        Dim pushable As Integer = second \ first  '' this is a quirk in vb that integer division is not the default behavior of /
+                        _stack.Push(pushable)
+                    Case Else
+                        Dim pushable = Integer.Parse(token)
+                        _stack.Push(pushable)
+                End Select
+            Next
+            Return _stack.Pop()
+        End Function
+    End Class
+
+End Namespace
