c6032bd13399fe43ddab2cd5820d14981d20091b
Jason <j-folkens@hotmail.com>
2025-11-15 10:35:53 -0500

    added interface for minstack (155)

---
+++ LeetCodeProblems.Interfaces/Medium/IMinStack_155.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    /// <summary>
+    /// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+    /// 
+    /// Implement the MinStack class:
+    /// 
+    /// MinStack() initializes the stack object.
+    /// void push(int val) pushes the element val onto the stack.
+    /// void pop() removes the element on the top of the stack.
+    /// int top() gets the top element of the stack.
+    /// int getMin() retrieves the minimum element in the stack.
+    /// You must implement a solution with O(1) time complexity for each function.
+    /// </summary>
+    public interface IMinStack_155
+    {
+        int GetMin();
+        void Pop();
+        void Push(int val);
+        int Top();
+    }
+}
