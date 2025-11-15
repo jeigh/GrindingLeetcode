00435f6d668b27aab007bd072c2b51d5c6bd1797
Jason <j-folkens@hotmail.com>
2025-11-15 10:34:01 -0500

    added interface for reverse polish notation

---
+++ LeetCodeProblems.Interfaces/Medium/IEvaluateReversePolishNotationCSharp_150.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeProblems.Interfaces.Medium
+{
+    /// <summary>
+    /// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+    /// 
+    /// Evaluate the expression.Return an integer that represents the value of the expression.
+    /// 
+    /// Note that:
+    /// 
+    /// The valid operators are '+', '-', '*', and '/'.
+    /// Each operand may be an integer or another expression.
+    /// The division between two integers always truncates toward zero.
+    /// There will not be any division by zero.
+    /// The input represents a valid arithmetic expression in a reverse polish notation.
+    /// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+    /// </summary>
+    public interface IEvaluateReversePolishNotationCSharp_150
+    {
+        int EvalRPN(string[] tokens);
+    }
+}
