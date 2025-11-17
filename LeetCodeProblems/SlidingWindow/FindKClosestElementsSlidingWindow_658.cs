a6f1602eec00279577aee2532daff54706377a8b
Jason <j-folkens@hotmail.com>
2025-11-17 08:25:00 -0500

    adding sliding window version of 658

---
+++ LeetCodeProblems/SlidingWindow/FindKClosestElementsSlidingWindow_658.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+
+namespace LeetCodeProblems.CSharp.SlidingWindow
+{    
+    public class FindKClosestElementsSlidingWindow_658 : IFindKClosestElements_658
+    {
+        // space complexity: O(k), time complexity O(n-k)
+        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            if (k >= arr.Length) return arr.ToList();
+
+            var left = 0;
+            var right = arr.Length - 1;
+
+            while (right - left >= k)
+            {
+                int leftDistance = Math.Abs(arr[left] - x);
+                int rightDistance = Math.Abs(arr[right] - x);
+
+                if (leftDistance > rightDistance)
+                    left++;
+                else if (leftDistance < rightDistance)
+                    right--;
+                else
+                    right--;
+            }
+
+            return arr[left..(right + 1)].ToList();
+        }
+    }
+}
