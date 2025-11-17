d258a243af5a805d497f7148de1c832737aec188
Jason <j-folkens@hotmail.com>
2025-11-17 08:24:29 -0500

    adding brute force version of 658

---
+++ LeetCodeProblems/BruteForce/FindKClosestElementsBruteForce_658.cs
@@ -0,0 +1,34 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+
+namespace LeetCodeProblems.CSharp.BruteForce
+{
+    public class FindKClosestElementsBruteForce_658 : IFindKClosestElements_658
+    {
+        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            var queue = new PriorityQueue<int, double>();
+
+            foreach(int i in arr)
+            {
+                queue.Enqueue(i, orderHack(x, i));
+            }
+
+            var returnable = new List<int>();
+            for (int i = 0; i < k; i++)
+            {
+                returnable.Add(queue.Dequeue());
+            }
+
+            returnable.Sort();
+
+            return returnable;
+        }
+
+        public double orderHack(int x, int i)
+        {
+            double retval = Math.Abs(x - i);
+            if (i > x) retval = retval + 0.1;
+            return retval;
+        }
+    }
+}
