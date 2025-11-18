672063b5ac75ccb3305165184c970a170d2b99e0
Jason <j-folkens@hotmail.com>
2025-11-18 07:21:26 -0500

    added interface for 739

---
+++ LeetCodeProblems.Interfaces/Medium/IDailyTemperatures_739.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeProblems.Interfaces.Medium
+{
+    /// <summary>
+    /// Given an array of integers temperatures represents the daily temperatures, 
+    /// return an array answer such that answer[i] is the number of days you have to wait 
+    /// after the ith day to get a warmer temperature. 
+    /// If there is no future day for which this is possible, keep answer[i] == 0 instead.
+    /// </summary>
+    public interface IDailyTemperatures_739
+    {
+        int[] DailyTemperatures(int[] temperatures);
+    }
+
+}
