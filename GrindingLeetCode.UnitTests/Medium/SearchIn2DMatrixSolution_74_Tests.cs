4ccf9f704909a95f3dc4c353fc4a84b4468a9a41
Jason <j-folkens@hotmail.com>
2025-11-23 08:48:11 -0500

    moved tests for 74

--- GrindingLeetCode.UnitTests/Medium/SearchIn2DMatrixSolution_74_Tests.cs
+++ GrindingLeetCode.UnitTests/Medium/SearchIn2DMatrixSolution_74_Tests.cs
@@ -1,7 +1,7 @@
 using LeetCodeProblems.BinarySearch;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace GrindingLeetCode.UnitTests.BinarySearch
+namespace GrindingLeetCode.UnitTests.Medium
 {
     [TestClass]
     public class SearchIn2DMatrixSolution_74_Tests
