c6098366c824fdf9695637070dace41a35b3b6ee
Jason <j-folkens@hotmail.com>
2025-11-26 09:23:02 -0500

    interface for 206

---
+++ LeetCodeProblems.Interfaces/Easy/IReverseLinkedList_206.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given the head of a singly linked list, reverse the list, and return the reversed list.
+    /// </summary>
+    public interface IReverseLinkedList_206
+    {
+        public ListNode ReverseList(ListNode head);
+    }
+}
