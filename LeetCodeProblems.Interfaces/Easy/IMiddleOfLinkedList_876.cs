b83e8295882734f225bd60f78e4427edd46593dc
Jason <j-folkens@hotmail.com>
2025-11-26 09:22:02 -0500

    interface for 876

---
+++ LeetCodeProblems.Interfaces/Easy/IMiddleOfLinkedList_876.cs
@@ -0,0 +1,14 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given the head of a singly linked list, return the middle node of the linked list.
+    /// 
+    /// If there are two middle nodes, return the second middle node.
+    /// </summary>
+    public interface IMiddleOfLinkedList_876
+    {
+        public ListNode MiddleNode(ListNode head);
+    }
+}
