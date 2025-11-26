80c43d2e3b46a14e7200447d17b02af242e5fd85
Jason <j-folkens@hotmail.com>
2025-11-26 09:21:42 -0500

    interface for 21

---
+++ LeetCodeProblems.Interfaces/Easy/IMergeTwoLists_21.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// You are given the heads of two sorted linked lists list1 and list2.
+    /// 
+    /// Merge the two lists into one sorted list.The list should be made by splicing together the nodes of the first two lists.
+    /// 
+    /// Return the head of the merged linked list.
+    /// </summary>
+    public interface IMergeTwoLists_21
+    {
+        ListNode MergeTwoLists(ListNode list1, ListNode list2);
+    }
+}
