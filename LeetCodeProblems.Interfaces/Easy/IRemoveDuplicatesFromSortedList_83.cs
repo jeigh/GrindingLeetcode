7cde033b8552bfb4ec9cd5bd9bfbe4b8a749562b
Jason <j-folkens@hotmail.com>
2025-11-26 09:22:39 -0500

    interface for 83

---
+++ LeetCodeProblems.Interfaces/Easy/IRemoveDuplicatesFromSortedList_83.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+    /// </summary>
+    public interface IRemoveDuplicatesFromSortedList_83
+    {
+        public ListNode DeleteDuplicates(ListNode head);
+    }
+}
