ffc317d2610f8524b4d67137482663f70d074329
Jason <j-folkens@hotmail.com>
2025-11-27 08:34:54 -0500

    lc 1876

---
+++ LeetCodeProblems/SlidingWindow/SubstringsOfSizeThreeWithDistinctCharacters_1876.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+
+namespace LeetCodeProblems.CSharp.SlidingWindow
+{
+    public class SubstringsOfSizeThreeWithDistinctCharacters_1876 : ISubstringsOfSizeThreeWithDistinctCharacters_1876
+    {
+        public int CountGoodSubstrings(string s)
+        {
+            int goodCount = 0;
+            for (int i = 0; i <= s.Length - 3; i++)
+            {
+                if (!HasRepeatingChars(s, i)) goodCount++;
+            }
+            return goodCount;
+        }
+
+        public bool HasRepeatingChars(string s, int start)
+        {
+            if (s[start] == s[start+1]) return true;
+            if (s[start + 1] == s[start + 2]) return true;
+            if (s[start] == s[start + 2]) return true;
+
+            return false;
+        }
+    }
+}
