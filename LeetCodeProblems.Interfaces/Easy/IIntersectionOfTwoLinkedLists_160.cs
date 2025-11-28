083a2e41bf815b267e899beccd27b3a8a59ea50c
Jason <j-folkens@hotmail.com>
2025-11-28 11:13:31 -0500

    interface for lc 160

---
+++ LeetCodeProblems.Interfaces/Easy/IIntersectionOfTwoLinkedLists_160.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+    /// 
+    /// The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+    /// 
+    /// Note that the linked lists must retain their original structure after the function returns.
+    /// </summary>
+    public interface IIntersectionOfTwoLinkedLists_160
+    {
+        ListNode GetIntersectionNode(ListNode headA, ListNode headB);
+    }
+}
