6483a0ed3af469d2df4ae434c7e83039ee5a4877
Jason <j-folkens@hotmail.com>
2025-11-28 11:19:38 -0500

    VB impl of 141

---
+++ LeetCodeProblems.VisualBasic/LinkedList/LinkedListCycleHashSetSolutionVB_141.vb
@@ -0,0 +1,24 @@
+﻿Imports LeetCodeProblems.Interfaces.Easy
+Imports LeetCodeProblems.Shared
+
+Namespace LinkedList
+    Public Class LinkedListCycleHashSetSolutionVB_141
+        Implements ILinkedListCycle_141
+
+        Private _hashset As New HashSet(Of ListNode)()
+
+        Public Function HasCycle(head As ListNode) As Boolean Implements ILinkedListCycle_141.HasCycle
+            Dim current As ListNode = head
+
+            While current IsNot Nothing
+
+                If _hashset.Contains(current) Then Return True
+                _hashset.Add(current)
+
+                current = current.next
+            End While
+
+            Return False
+        End Function
+    End Class
+End Namespace
