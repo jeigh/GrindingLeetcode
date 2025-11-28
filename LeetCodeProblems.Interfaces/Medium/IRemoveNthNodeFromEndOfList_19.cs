748a01543253c06d39a308359d538a313a54fb24
Jason <j-folkens@hotmail.com>
2025-11-28 11:13:55 -0500

    interface for lc 19

---
+++ LeetCodeProblems.Interfaces/Medium/IRemoveNthNodeFromEndOfList_19.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    /// <summary>
+    /// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+    /// </summary>
+    public interface IRemoveNthNodeFromEndOfList_19
+    {
+        ListNode RemoveNthFromEnd(ListNode head, int n);
+    }
+}
