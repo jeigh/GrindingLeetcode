433cbe26afad49cc080c4d570f3461cde6b46f98
Jason <j-folkens@hotmail.com>
2025-11-29 09:36:25 -0500

    lc 232

---
+++ LeetCodeProblems/Stack/ImplementQueueUsingStacks_232.cs
@@ -0,0 +1,49 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+
+namespace LeetCodeProblems
+{
+    public class ImplementQueueUsingStacks_232 : IImplementQueueUsingStacks_232
+    {
+        public Stack<int> stack = new();
+        public Stack<int> stack2 = new();  
+
+        public bool Empty()
+        {
+            return stack.Count == 0;
+        }
+
+        public int Peek()
+        {
+            while (stack.Count > 1)
+            {
+                stack2.Push(stack.Pop());
+            }
+            var returnable = stack.Peek();
+            while (stack2.Count > 0)
+            {
+                stack.Push(stack2.Pop());
+            }
+            return returnable;
+        }
+
+        public int Pop()
+        {
+            while (stack.Count > 1)
+            {
+                stack2.Push(stack.Pop());
+            }
+            var returnable = stack.Pop();
+            while (stack2.Count > 0)
+            {
+                stack.Push(stack2.Pop());
+            }
+            return returnable;
+
+        }
+
+        public void Push(int x)
+        {
+            stack.Push(x);
+        }
+    }
+}
