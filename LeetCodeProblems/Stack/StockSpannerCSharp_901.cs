eca042d3aa04ef98ac9a33610c0a44e0797aee3c
Jason <j-folkens@hotmail.com>
2025-11-29 09:36:47 -0500

    lc 901

---
+++ LeetCodeProblems/Stack/StockSpannerCSharp_901.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+
+namespace LeetCodeProblems.CSharp.Stack
+{
+    public class StockSpannerCSharp_901 : IStockSpanner_901
+    {
+        private Stack<(int price, int span)> _stack = new Stack<(int price, int span)>();
+
+        public int Next(int price)
+        {
+            if (_stack.Count == 0)
+            {
+                _stack.Push((price, 1));
+                return 1;
+            }
+
+            var span = 1;
+            while (_stack.Count > 0 && _stack.Peek().price <= price)
+            {
+                var popped = _stack.Pop();
+                span += popped.span;
+            }
+            _stack.Push((price, span));
+            return span;
+        }
+    }
+}
