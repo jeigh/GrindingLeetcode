9e1fddaf2f27814e2dcbea7baaf4644e51231954
Jason <j-folkens@hotmail.com>
2025-11-30 09:19:47 -0500

    adding LC 237

---
+++ LeetCodeProblems/LinkedList/DeleteNodeInALinkedListCSharp_237.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.LinkedList
+{
+    public class DeleteNodeInALinkedListCSharp_237 : IDeleteNodeInALinkedList_237
+    {
+        public void DeleteNode(ListNode node)
+        {
+            if (node.next == null) node = null;
+            else
+            {
+                var nextNode = node.next;
+                node.next = nextNode.next;
+                node.val = nextNode.val;
+                nextNode = null;
+            }
+        }
+    }
+}
