6bbcfa3f396c90d5a82d450f4081da24d828a6b6
Jason <j-folkens@hotmail.com>
2025-11-30 09:43:32 -0500

    lc 141 hashmap solution

---
+++ LeetCodeProblems/LinkedList/LinkedListCycleHashmapCSharpSolution_141.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.LinkedList
+{
+    public class LinkedListCycleHashmapCSharpSolution_141 : ILinkedListCycle_141
+    {
+        // time complexity: O(n)
+        // space complexity: O(n)
+        public bool HasCycle(ListNode head)
+        {
+            var hashSet = new HashSet<ListNode>();
+            ListNode current = head;
+
+            while (true)
+            {
+                if (current == null) return false;
+                if (hashSet.Contains(current)) return true;
+                else hashSet.Add(current);
+                current = current.next;
+            }
+        }
+    }
+}
