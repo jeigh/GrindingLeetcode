20dbdf49effc683abcdd0c63bf3e8d3f719f664e
Jason <j-folkens@hotmail.com>
2025-11-30 09:22:42 -0500

    optimized impl of 160

---
+++ LeetCodeProblems/LinkedList/IntersectionOfTwoLinkedListOptimizedCSharp_160.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.LinkedList
+{
+    public class IntersectionOfTwoLinkedListOptimizedCSharp_160 : IIntersectionOfTwoLinkedLists_160
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            var nodeA = headA;
+            var nodeB = headB;
+
+            while (nodeA != nodeB)
+            {
+                if (nodeA.next != null) nodeA = nodeA.next; 
+                else nodeA = headB;
+
+                if (nodeB.next != null) nodeB = nodeB.next;
+                else nodeB = headA;
+            }
+
+            return nodeA;
+        }
+    }
+}
