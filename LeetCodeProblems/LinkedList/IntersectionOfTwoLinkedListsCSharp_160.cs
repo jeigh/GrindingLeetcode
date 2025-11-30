3b66f4f25aa53e5e2976569129caddda93e4e14c
Jason <j-folkens@hotmail.com>
2025-11-30 09:20:49 -0500

    adding lc 160

---
+++ LeetCodeProblems/LinkedList/IntersectionOfTwoLinkedListsCSharp_160.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.LinkedList
+{
+    public class IntersectionOfTwoLinkedListsUsingHashmapCSharp_160 : IIntersectionOfTwoLinkedLists_160
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            var hashSet = new HashSet<ListNode>();
+            var current = headA;
+
+            while (current != null)
+            {
+                hashSet.Add(current);
+                current = current.next;
+            }
+
+            current = headB;
+            while (!hashSet.Contains(current) && current != null)
+            {
+                current = current.next;                
+            }
+            return current;
+        }
+    }
+}
