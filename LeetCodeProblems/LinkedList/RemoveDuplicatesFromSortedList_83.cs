e72c0e080af7af11132d5aa3742f75ea4e3b324a
Jason <j-folkens@hotmail.com>
2025-12-01 08:57:28 -0500

    lc 83

---
+++ LeetCodeProblems/LinkedList/RemoveDuplicatesFromSortedList_83.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.HashingOrArrays;
+using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.LinkedList
+{
+    public class RemoveDuplicatesFromSortedList_83 : IRemoveDuplicatesFromSortedList_83
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null) return null;
+
+            ListNode current = head;
+
+            while (current != null && current.next != null)
+            {
+                if (current.val == current.next.val)
+                    current.next = current.next.next;
+                else
+                    current = current.next;
+            }
+
+            return head;
+        }
+    }
+}
