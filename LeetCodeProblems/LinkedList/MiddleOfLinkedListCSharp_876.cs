f6cd339dd9d5cdab778573faf9a5571868e84140
Jason <j-folkens@hotmail.com>
2025-12-01 08:57:09 -0500

    lc 876

---
+++ LeetCodeProblems/LinkedList/MiddleOfLinkedListCSharp_876.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.LinkedList
+{
+    public class MiddleOfLinkedListCSharp_876 : IMiddleOfLinkedList_876
+    {
+        public ListNode MiddleNode(ListNode node)
+        {
+            var fast = node;
+            var slow = node;
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next; 
+                slow = slow.next;
+            }
+
+            return slow;
+        }
+    }
+}
