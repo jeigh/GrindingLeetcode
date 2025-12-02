70685628294652e3eea302475b3f5652dbff1c1f
Jason <j-folkens@hotmail.com>
2025-12-02 08:10:14 -0500

    middle of linked list

---
+++ LeetCodeProblems.VisualBasic/LinkedList/MiddleOfLinkedListVB_876.vb
@@ -0,0 +1,20 @@
+﻿Imports LeetCodeProblems.Interfaces.Easy
+Imports LeetCodeProblems.Shared
+
+Namespace LinkedList
+    Public Class MiddleOfLinkedListVB_876
+        Implements IMiddleOfLinkedList_876
+
+        Public Function MiddleNode(head As ListNode) As ListNode Implements IMiddleOfLinkedList_876.MiddleNode
+            Dim fast = head
+            Dim slow = head
+
+            While fast?.next IsNot Nothing
+                fast = fast?.next?.next
+                slow = slow.next
+            End While
+
+            Return slow
+        End Function
+    End Class
+End Namespace
