e430c541c9f326268a24273d1d3d102b9c7c4367
Jason <j-folkens@hotmail.com>
2025-12-05 08:57:58 -0500

    added node class

--- LeetCodeProblems.Interfaces/ListNode.cs
+++ LeetCodeProblems.Interfaces/ListNode.cs
@@ -10,4 +10,18 @@
             this.next = next;
         }
     }
+
+    public class Node
+    {
+        public int val;
+        public Node? next;
+        public Node? random;
+
+        public Node(int _val)
+        {
+            val = _val;
+            next = null;
+            random = null;
+        }
+    }
 }
