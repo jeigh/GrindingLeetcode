b1914bd32472f29c7f0776934b95ed757eff2e4b
Jason <j-folkens@hotmail.com>
2025-12-07 13:30:29 -0500

    interface for 92

---
+++ LeetCodeProblems.Interfaces/Medium/IReverseLinkedListII_92.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    /// <summary>
+    /// Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+    /// </summary>
+    public interface IReverseLinkedListII_92
+    {
+        ListNode ReverseBetween(ListNode head, int left, int right);
+    }
+}
