9f1d1caef2a8fc7c4e778bf6670dc4a512548b0c
Jason <j-folkens@hotmail.com>
2025-12-09 07:59:45 -0500

    reverse and reconnect c#

---
+++ LeetCodeProblems/LinkedList/ReverseLinkedListII_ReversalAndReconnectCSharp_92.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.LinkedList
+{
+    public class ReverseLinkedListII_ReversalAndReconnectCSharp_92 : IReverseLinkedListII_92
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            var dummy = new ListNode(0, head);
+            var leftNode = dummy;
+            var current = head;
+
+            for (var i = 0; i < left - 1; i++)
+            {
+                leftNode = current;
+                current = current.next;
+            }
+
+            ListNode? prev = null;
+            var remainingIterationCount = right - left + 1;
+            while (remainingIterationCount > 0)
+            {
+                var temp = current.next;
+
+                current.next = prev;
+                prev = current;
+                current = temp;
+
+                remainingIterationCount--;
+            }
+
+            leftNode.next.next = current;
+            leftNode.next = prev;
+
+            return dummy.next;
+        }
+    }
+}
