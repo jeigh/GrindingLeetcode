9336ca9582cad40d63d5509f070ab7111abee233
Jason <j-folkens@hotmail.com>
2025-12-10 16:34:36 -0500

    Reverse List II (move to front) cs

---
+++ LeetCodeProblems/LinkedList/ReverseLinkedListII_MoveToFrontCSharp_92.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.LinkedList
+{
+    public class ReverseLinkedListII_MoveToFrontCSharp_92 : IReverseLinkedListII_92
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            var dummy = new ListNode(0, head);
+            var prev = dummy;
+
+            for (var i = 0; i < left - 1; i++)
+            {
+                prev = prev.next;
+            }
+
+            var current = prev.next;
+
+            var remainingIterationCount = right - left;
+            while (remainingIterationCount > 0)
+            {
+                var temp = current.next;
+
+                current.next = temp.next;
+                temp.next = prev.next;
+
+                prev.next = temp;
+
+                remainingIterationCount--;
+            }
+
+            return dummy.next;
+        }
+    }
+}
