3a4c1a6926bfafc9d41230544c42a12f753f6bc1
Jason <j-folkens@hotmail.com>
2025-12-13 12:07:58 -0500

    added interface for 94

---
+++ LeetCodeProblems.Interfaces/Easy/IBinaryTreeInorderTraversal_94.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+    /// </summary>
+    public interface IBinaryTreeInorderTraversal_94
+    {
+        IList<int> InorderTraversal(TreeNode root);
+    }
+}
