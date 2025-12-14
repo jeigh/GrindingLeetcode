8a8ba3f7e6ea47d22b7b1d884d843ed4a8a431c1
Jason <j-folkens@hotmail.com>
2025-12-14 12:24:50 -0500

    added interface for LC 145

---
+++ LeetCodeProblems.Interfaces/Easy/IBinaryTreePostOrderTraversal_145.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+    /// </summary>
+    public interface IBinaryTreePostOrderTraversal_145
+    {
+        IList<int> PostOrderTraversal(TreeNode root);
+    }
+
+}
