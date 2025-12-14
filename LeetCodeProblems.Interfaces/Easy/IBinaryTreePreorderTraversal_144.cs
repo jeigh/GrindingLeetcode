0f9bbfe0ff6d16d6ae3f58ba79d80c2608219abd
Jason <j-folkens@hotmail.com>
2025-12-14 12:25:12 -0500

    Added interface for LC 144

---
+++ LeetCodeProblems.Interfaces/Easy/IBinaryTreePreorderTraversal_144.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+    /// </summary>
+    public interface IBinaryTreePreorderTraversal_144
+    {
+        IList<int> PreorderTraversal(TreeNode root);
+    }
+
+}
