6a9b20ab43471bf4202fedb56ce7afae4649dcac
Jason <j-folkens@hotmail.com>
2025-12-15 09:28:37 -0500

    updated 226 to reflect the new namespace

--- LeetCodeProblems/Tree/InvertBinaryTree_Recursive_CSharp_226.cs
+++ LeetCodeProblems/Tree/InvertBinaryTree_Recursive_CSharp_226.cs
@@ -1,8 +1,10 @@
-﻿using LeetCodeProblems.Shared;
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
 
-namespace LeetCodeProblems
+namespace LeetCodeProblems.CSharp.Tree
 {
-    public class InvertBinaryTreeSolution
+    // solved 2025-07-02
+    public class InvertBinaryTree_Recursive_CSharp_226 : IInvertBinaryTree_226
     {
         // space complexity: O(h);  time complexity: O(n)
         // where h is the height of the tree, and n is the number of nodes in the tree
