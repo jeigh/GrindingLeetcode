e1925d2f3708bcbd0d7ab16922124862492316dd
Jason <j-folkens@hotmail.com>
2025-12-15 09:31:02 -0500

    IDoubleListNode (for LRU 146 and 622)

---
+++ LeetCodeProblems/LinkedList/IDoubleListNode.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeProblems.CSharp.LinkedList
+{
+    public interface IDoubleListNode
+    {
+        DoubleListNode Next { get; }
+        DoubleListNode Prev { get; }
+        int Value { get; set; }
+
+        void SetNext(DoubleListNode? nextNode);
+        void SetPrev(DoubleListNode? prevNode);
+    }
+}
