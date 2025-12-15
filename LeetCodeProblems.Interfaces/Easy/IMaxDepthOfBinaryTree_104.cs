3570286176c996b891e8b7f9c0b2baab58fa6d38
Jason <j-folkens@hotmail.com>
2025-12-15 09:29:48 -0500

    interface for 104

---
+++ LeetCodeProblems.Interfaces/Easy/IMaxDepthOfBinaryTree_104.cs
@@ -0,0 +1,14 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given the root of a binary tree, return its maximum depth.
+    ///
+    /// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+    /// </summary>
+    public interface IMaxDepthOfBinaryTree_104
+    {
+        int MaxDepth(TreeNode root);
+    }
+}
