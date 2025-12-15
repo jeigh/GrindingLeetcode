2ecafb3fc35d30311dafcdd2a4b905239fb35d2f
Jason <j-folkens@hotmail.com>
2025-12-15 09:29:32 -0500

    interface for 226

---
+++ LeetCodeProblems.Interfaces/Easy/IInvertBinaryTree_226.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given the root of a binary tree, invert the tree, and return its root.
+    /// </summary>
+    public interface IInvertBinaryTree_226
+    {
+        TreeNode InvertTree(TreeNode root);
+    }
+}
