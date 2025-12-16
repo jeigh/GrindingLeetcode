38c070753bc5e84d98ff5445a18eca056523cc59
Jason <j-folkens@hotmail.com>
2025-12-16 15:41:16 -0500

    recursive vb edition of LC 94

---
+++ LeetCodeProblems.VisualBasic/Tree/BinaryTreeInOrderTraversal_Recursive_94.vb
@@ -0,0 +1,17 @@
+﻿Imports LeetCodeProblems.Interfaces.Easy
+Imports LeetCodeProblems.Shared
+
+Public Class BinaryTreeInOrderTraversal_Recursive_VB_94
+    Implements IBinaryTreeInorderTraversal_94
+
+    Public Function InorderTraversal(root As TreeNode) As IList(Of Integer) Implements IBinaryTreeInorderTraversal_94.InorderTraversal
+        If root Is Nothing Then Return New List(Of Integer)()
+
+        Dim returnable As List(Of Integer) = InorderTraversal(root.left)
+        returnable.Add(root.val)
+        returnable.AddRange(InorderTraversal(root.right))
+
+        Return returnable
+    End Function
+End Class
+
