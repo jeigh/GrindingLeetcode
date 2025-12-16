e5e5fa99b3f5e0f96ad60d662eac5af8c9496aed
Jason <j-folkens@hotmail.com>
2025-12-16 15:40:40 -0500

    vb edition of LC 92

---
+++ LeetCodeProblems.VisualBasic/LinkedList/ReverseLinkedListII_MoveToFrontVB_92.vb
@@ -0,0 +1,30 @@
+﻿Imports LeetCodeProblems.Interfaces.Medium
+Imports LeetCodeProblems.Shared
+
+Namespace LinkedList
+    Public Class ReverseLinkedListII_MoveToFrontVB_92
+        Implements IReverseLinkedListII_92
+
+        Public Function ReverseBetween(head As ListNode, left As Integer, right As Integer) As ListNode Implements IReverseLinkedListII_92.ReverseBetween
+
+            Dim dummyForHead As New ListNode(0, head)
+            Dim leftBoundary = dummyForHead
+
+            For index = 1 To left - 1
+                leftBoundary = leftBoundary.next
+            Next
+
+            Dim anchor = leftBoundary.next
+
+            For index = left To right - 1
+                Dim nextNode = anchor.next
+
+                anchor.next = anchor.next.next
+                nextNode.next = leftBoundary.next
+                leftBoundary.next = nextNode
+            Next
+
+            Return dummyForHead.next
+        End Function
+    End Class
+End Namespace
