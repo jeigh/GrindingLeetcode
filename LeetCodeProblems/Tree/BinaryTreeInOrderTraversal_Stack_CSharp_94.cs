e34d0a3091232cb6a1d145fa895db149ae06a37d
Jason <j-folkens@hotmail.com>
2025-12-18 16:55:28 -0500

    LC 94 Csharp with stack

---
+++ LeetCodeProblems/Tree/BinaryTreeInOrderTraversal_Stack_CSharp_94.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class BinaryTreeInOrderTraversal_Stack_CSharp_94 : IBinaryTreeInorderTraversal_94
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var stack = new Stack<TreeNode>();
+            var returnable = new List<int>();
+            var currentNode = root;
+
+            while (currentNode != null || stack.Any())
+            {
+                while (currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.left;
+                }
+
+                currentNode = stack.Pop();
+                returnable.Add(currentNode.val);
+                currentNode = currentNode.right;
+            }
+
+            return returnable;
+        }
+    }
+}
