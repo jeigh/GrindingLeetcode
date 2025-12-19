dc9cf871f099c47a78caec0f3e499bf9fcec9638
Jason <j-folkens@hotmail.com>
2025-12-19 11:09:25 -0500

    LC 226

---
+++ LeetCodeProblems/Tree/InvertBinaryTree_Stack_CSharp_226.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+
+    public class InvertBinaryTree_Stack_CSharp_226 : IInvertBinaryTree_226
+    {
+        // solved on 2025-12-14
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return null;
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                
+                if (current.left != null) stack.Push(current.left);
+                if (current.right != null) stack.Push(current.right);
+
+                var temp = current.left;
+                current.left = current.right;
+                current.right = temp;
+            }
+
+            return root;            
+        }
+    }
+}
