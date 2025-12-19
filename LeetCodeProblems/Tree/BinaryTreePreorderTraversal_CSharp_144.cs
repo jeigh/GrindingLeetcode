2620f89854c17ec51195d129e117ca2cf564600c
Jason <j-folkens@hotmail.com>
2025-12-19 11:04:46 -0500

    LC 144

---
+++ LeetCodeProblems/Tree/BinaryTreePreorderTraversal_CSharp_144.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class BinaryTreePreorderTraversal_CSharp_144 : IBinaryTreePreorderTraversal_144
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            if (root == null) return new List<int>();
+            var stack = new Stack<TreeNode>();
+            var result = new List<int>();
+            var current = root;
+            while (stack.Any() || current != null)
+            {
+                while (current != null)
+                {
+                    result.Add(current.val);
+                    stack.Push(current);
+                    current = current.left;
+                }
+                current = stack.Pop();
+                current = current.right;
+            }
+            return result;
+        }
+    }
+}
