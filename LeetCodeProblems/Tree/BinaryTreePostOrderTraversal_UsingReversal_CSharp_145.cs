8dc2a7106e546ccf589809087cab74965ca646a3
Jason <j-folkens@hotmail.com>
2025-12-20 11:03:06 -0500

    LC 145 Reversal C#

---
+++ LeetCodeProblems/Tree/BinaryTreePostOrderTraversal_UsingReversal_CSharp_145.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class BinaryTreePostOrderTraversal_UsingReversal_CSharp_145 : IBinaryTreePostOrderTraversal_145
+    {
+        // solved on 2025-12-14
+        public IList<int> PostOrderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            var stack = new Stack<TreeNode>();
+            var current = root;
+
+            while (current != null || stack.Any())
+            {
+                while (current != null)
+                {
+                    result.Add(current.val);
+                    stack.Push(current);
+                    current = current.right;
+                }
+
+                current = stack.Pop();
+                current = current.left;                
+            }
+
+            result.Reverse();
+            return result;
+
+        }
+    }
+}
