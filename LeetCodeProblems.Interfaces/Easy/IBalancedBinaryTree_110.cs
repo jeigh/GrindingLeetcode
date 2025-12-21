1ce73716b7c8bf36db9154960d6a2d40ddc03941
Jason <j-folkens@hotmail.com>
2025-12-21 09:57:09 -0500

    interface for LC 110

---
+++ LeetCodeProblems.Interfaces/Easy/IBalancedBinaryTree_110.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    /// <summary>
+    /// Given a binary tree, determine if it is height-balanced.
+    /// </summary>
+    public interface IBalancedBinaryTree_110
+    {
+        bool IsBalanced(TreeNode root);
+    }
+}
