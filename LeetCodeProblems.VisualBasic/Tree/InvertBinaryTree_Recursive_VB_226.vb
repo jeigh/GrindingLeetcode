39748bda7bb92858ef744d1c7f76b71fb92275f0
Jason <j-folkens@hotmail.com>
2025-12-22 10:09:06 -0500

    LC 226 Recursive VB

---
+++ LeetCodeProblems.VisualBasic/Tree/InvertBinaryTree_Recursive_VB_226.vb
@@ -0,0 +1,20 @@
+﻿Imports LeetCodeProblems.Interfaces.Easy
+Imports LeetCodeProblems.Shared
+
+Public Class InvertBinaryTree_Recursive_VB_226
+    Implements IInvertBinaryTree_226
+
+    ' solved on 2025-12-19
+    Public Function InvertTree(root As TreeNode) As TreeNode Implements IInvertBinaryTree_226.InvertTree
+        If root Is Nothing Then Return Nothing
+        If (root.left Is Nothing AndAlso root.right Is Nothing) Then Return root
+
+        Dim temp = root.left
+        root.left = InvertTree(root.right)
+        root.right = InvertTree(temp)
+
+        Return root
+    End Function
+End Class
+
+
