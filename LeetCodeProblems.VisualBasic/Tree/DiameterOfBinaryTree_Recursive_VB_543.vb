a36ab545b2da7752f58e4d41214534586a189107
Jason <j-folkens@hotmail.com>
2025-12-26 09:14:40 -0500

    LC 543 recursive VB

---
+++ LeetCodeProblems.VisualBasic/Tree/DiameterOfBinaryTree_Recursive_VB_543.vb
@@ -0,0 +1,26 @@
+﻿Imports LeetCodeProblems.Interfaces.Easy
+Imports LeetCodeProblems.Shared
+
+Public Class DiameterOfBinaryTree_Recursive_VB_543
+    Implements IDiameterOfBinaryTree_543
+
+    ' solved 2025-12-19
+    Public Function DiameterOfBinaryTree(root As TreeNode) As Integer Implements IDiameterOfBinaryTree_543.DiameterOfBinaryTree
+        Dim maxDiameter As Integer = 0
+        MaxDepth(root, maxDiameter)
+        Return maxDiameter
+    End Function
+
+    Private Function MaxDepth(node As TreeNode, ByRef diameter As Integer) As Integer
+        If node Is Nothing Then Return 0
+
+        Dim leftDepth = MaxDepth(node.left, diameter)
+        Dim rightDepth = MaxDepth(node.right, diameter)
+
+        ' depth calculation
+        diameter = Math.Max(diameter, leftDepth + rightDepth)
+
+        ' depth calculation
+        Return 1 + Math.Max(leftDepth, rightDepth)
+    End Function
+End Class
