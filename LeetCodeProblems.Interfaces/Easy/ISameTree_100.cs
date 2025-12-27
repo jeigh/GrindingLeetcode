44ca67dc44425ea9ac1515c42ebe4c5880d92a79
Jason <j-folkens@hotmail.com>
2025-12-27 12:39:03 -0500

    interface for lc 100

---
+++ LeetCodeProblems.Interfaces/Easy/ISameTree_100.cs
@@ -0,0 +1,9 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    public interface ISameTree_100
+    {
+        bool IsSameTree(TreeNode p, TreeNode q);
+    }
+}
