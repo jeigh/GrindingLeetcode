f6e5af850ec33ab5b835aa83e489ce4d0cd53bf5
Jason <j-folkens@hotmail.com>
2025-12-28 13:04:48 -0500

    interface for 235

---
+++ LeetCodeProblems.Interfaces/Medium/ILowestCommonAncestorOfBinarySearchTree_235.cs
@@ -0,0 +1,9 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface ILowestCommonAncestorOfBinarySearchTree_235
+    {
+        TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q);
+    }        
+}
