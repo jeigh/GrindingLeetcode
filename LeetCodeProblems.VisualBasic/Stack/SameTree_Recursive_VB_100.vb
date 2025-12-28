55c68dcc738c74ae2c28cfdab50e842386b50aa3
Jason <j-folkens@hotmail.com>
2025-12-28 13:06:12 -0500

    sametree recursive vb

---
+++ LeetCodeProblems.VisualBasic/Stack/SameTree_Recursive_VB_100.vb
@@ -0,0 +1,19 @@
+﻿Imports LeetCodeProblems.Interfaces.Easy
+Imports LeetCodeProblems.Shared
+
+' solved 2025-12-26
+Public Class SameTree_Recursive_VB_100
+    Implements ISameTree_100
+    Public Function IsSameTree(p As TreeNode, q As TreeNode) As Boolean Implements ISameTree_100.IsSameTree
+        If p Is Nothing AndAlso q Is Nothing Then Return True
+        If p Is Nothing OrElse q Is Nothing Then Return False
+
+        If p.val <> q.val Then Return False
+
+        If Not IsSameTree(p.left, q.left) Then Return False
+        If Not IsSameTree(p.right, q.right) Then Return False
+
+        Return True
+    End Function
+End Class
+
