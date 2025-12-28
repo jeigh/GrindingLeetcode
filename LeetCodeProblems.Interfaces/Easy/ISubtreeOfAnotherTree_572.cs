72479b90a377147e9e146c744ae3e0833cd14e0c
Jason <j-folkens@hotmail.com>
2025-12-28 13:04:28 -0500

    interface for 572

---
+++ LeetCodeProblems.Interfaces/Easy/ISubtreeOfAnotherTree_572.cs
@@ -0,0 +1,9 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    public interface ISubtreeOfAnotherTree_572
+    {
+        bool IsSubtree(TreeNode root, TreeNode subroot);
+    }
+}
