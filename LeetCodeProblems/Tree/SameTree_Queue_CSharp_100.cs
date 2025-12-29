62647718ef7625d6e4fc66b2c21aad4c2f27e3e1
Jason <j-folkens@hotmail.com>
2025-12-29 09:38:44 -0500

    LC 100, BFS

---
+++ LeetCodeProblems/Tree/SameTree_Queue_CSharp_100.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    // solved 2025-12-24
+    public class SameTree_Queue_CSharp_100 : ISameTree_100
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            var queue = new Queue<(TreeNode?, TreeNode?)>();
+            queue.Enqueue((p, q));
+
+            while (queue.Count > 0)
+            {
+                (var pNode, var qNode) = queue.Dequeue();
+                if (pNode == null && qNode == null) continue;
+                if (pNode?.val != qNode?.val) return false;
+
+                queue.Enqueue((pNode?.left, qNode?.left));
+                queue.Enqueue((pNode?.right, qNode?.right));
+                
+            }
+            return true;
+        }
+    }
+}
