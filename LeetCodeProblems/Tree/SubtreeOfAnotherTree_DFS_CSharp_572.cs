c207c21ad4d6d06f131c86ca093b51a40adfc827
Jason <j-folkens@hotmail.com>
2025-12-30 08:27:48 -0500

    renamed/revised 572

---
+++ LeetCodeProblems/Tree/SubtreeOfAnotherTree_DFS_CSharp_572.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class SubtreeOfAnotherTree_DFS_CSharp_572 : ISubtreeOfAnotherTree_572
+    {
+        // solved 2025-12-25 (christmas)
+        public bool IsSubtree(TreeNode root, TreeNode subroot)
+        {
+            if (subroot == null) return true;
+            if (root == null) return false;
+            if (IsSameTree(root, subroot)) return true;
+            return IsSubtree(root?.left, subroot) || IsSubtree(root?.right, subroot);
+        }
+
+        public bool IsSameTree(TreeNode? p, TreeNode? q)
+        {
+            if (p == null && q == null) return true;
+            if (p == null || q == null) return false;
+            if (p.val != q.val) return false;
+
+            if (!IsSameTree(p?.left, q?.left)) return false;
+            if (!IsSameTree(p?.right, q?.right)) return false;
+            return true;
+        }
+
+    }
+}
