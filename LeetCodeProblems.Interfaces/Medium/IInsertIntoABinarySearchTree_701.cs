1f0caa7353b39a93f2e55576eac8248d6f44da8c
Jason <j-folkens@hotmail.com>
2025-12-31 09:04:18 -0500

    LC 701 interface

---
+++ LeetCodeProblems.Interfaces/Medium/IInsertIntoABinarySearchTree_701.cs
@@ -0,0 +1,9 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface IInsertIntoABinarySearchTree_701
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val);
+    }
+}
