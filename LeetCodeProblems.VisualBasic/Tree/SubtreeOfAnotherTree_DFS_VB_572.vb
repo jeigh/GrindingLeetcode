e2ad42ed050388117058818a3176b7d4e9f0616c
Jason <j-folkens@hotmail.com>
2026-01-01 12:47:40 -0500

    Add VB DFS solution for LeetCode 572 Subtree problem
    Implemented SubtreeOfAnotherTree_DFS_VB_572 class with IsSubtree and IsSameTree methods using depth-first search. Class conforms to ISubtreeOfAnotherTree_572 interface and includes necessary imports.

---
+++ LeetCodeProblems.VisualBasic/Tree/SubtreeOfAnotherTree_DFS_VB_572.vb
@@ -0,0 +1,31 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports LeetCodeProblems.Interfaces.Easy
+Imports LeetCodeProblems.Shared
+
+Public Class SubtreeOfAnotherTree_DFS_VB_572
+    Implements ISubtreeOfAnotherTree_572
+
+    Public Function IsSubtree(root As TreeNode, subroot As TreeNode) As Boolean Implements ISubtreeOfAnotherTree_572.IsSubtree
+        If subroot Is Nothing Then Return True
+        If root Is Nothing Then Return False
+
+        If IsSameTree(root, subroot) Then Return True
+
+        If IsSubtree(root.left, subroot) Then Return True
+        If IsSubtree(root.right, subroot) Then Return True
+        Return False
+    End Function
+
+    Private Function IsSameTree(p As TreeNode, q As TreeNode) As Boolean
+        If p Is Nothing AndAlso q Is Nothing Then Return True
+        If p Is Nothing OrElse q Is Nothing Then Return False
+
+        If p.val <> q.val Then Return False
+        Return IsSameTree(p.left, q.left) AndAlso IsSameTree(p.right, q.right)
+    End Function
+
+
+
+
+End Class
+
