2a3a88cffe66612002f9a2d8615ed34522536bac
Jason <j-folkens@hotmail.com>
2026-01-01 12:47:07 -0500

    Add VB implementation for SubtreeOfAnotherTree (serialization)
    Implemented SubtreeOfAnotherTree in VB using tree serialization.
    IsSubtree checks if serialized subroot is in serialized root.
    Serialize uses "$" for node values and "#" for nulls.
    Includes necessary imports and interface implementation.

---
+++ LeetCodeProblems.VisualBasic/Tree/SubtreeOfAnotherTree_Serialization_VB_572.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Text
+Imports LeetCodeProblems.Interfaces.Easy
+Imports LeetCodeProblems.Shared
+
+Public Class SubtreeOfAnotherTree_Serialization_VB_572
+    Implements ISubtreeOfAnotherTree_572
+
+    Public Function IsSubtree(root As TreeNode, subroot As TreeNode) As Boolean Implements ISubtreeOfAnotherTree_572.IsSubtree
+        If subroot Is Nothing Then Return True
+        If root Is Nothing And subroot IsNot Nothing Then Return False
+        If Serialize(root).Contains(Serialize(subroot)) Then Return True
+        Return False
+    End Function
+
+    Private Function Serialize(root As TreeNode) As String
+        If root Is Nothing Then Return "$#"
+        Return $"${root.val}{Serialize(root.left)}${Serialize(root.right)}"
+    End Function
+End Class
+
