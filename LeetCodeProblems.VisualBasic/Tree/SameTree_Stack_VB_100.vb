a206780fb394fcdaca87973ea298b1a7e3d007ed
Jason <j-folkens@hotmail.com>
2026-01-01 12:46:30 -0500

    Add iterative stack-based solution for Same Tree (VB)
    Implemented SameTree_Stack_VB_100 class using a stack to compare two binary trees for equality. The class adheres to the ISameTree_100 interface and includes necessary imports and a solved date comment.

---
+++ LeetCodeProblems.VisualBasic/Tree/SameTree_Stack_VB_100.vb
@@ -0,0 +1,28 @@
+﻿Imports LeetCodeProblems.Interfaces.Easy
+Imports LeetCodeProblems.Shared
+
+' solved 2025-12-26
+Public Class SameTree_Stack_VB_100
+    Implements ISameTree_100
+
+    Public Function IsSameTree(p As TreeNode, q As TreeNode) As Boolean Implements ISameTree_100.IsSameTree
+        Dim stack As New Stack(Of (pNode As TreeNode, qNode As TreeNode))
+        stack.Push((p, q))
+
+        While stack.Count > 0
+            Dim popped = stack.Pop()
+            Dim qNode = popped.qNode
+            Dim pNode = popped.pNode
+
+            If pNode Is Nothing AndAlso qNode Is Nothing Then Continue While
+            If pNode Is Nothing OrElse qNode Is Nothing Then Return False
+            If pNode.val <> qNode.val Then Return False
+
+            stack.Push((pNode.left, qNode.left))
+            stack.Push((pNode.right, qNode.right))
+        End While
+
+        Return True
+    End Function
+End Class
+
