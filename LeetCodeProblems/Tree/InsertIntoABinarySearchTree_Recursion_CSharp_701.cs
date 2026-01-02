daa15cdb2eab6f89baf23205cafb36071e466a89
Jason <j-folkens@hotmail.com>
2026-01-02 09:37:11 -0500

    Add recursive BST insertion class for LeetCode 701

---
+++ LeetCodeProblems/Tree/InsertIntoABinarySearchTree_Recursion_CSharp_701.cs
@@ -0,0 +1,21 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class InsertIntoABinarySearchTree_Recursion_CSharp_701 : IInsertIntoABinarySearchTree_701
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null) return new TreeNode(val);
+
+            if (val > root.val && root.right != null) InsertIntoBST(root.right, val);
+            else if (val > root.val) root.right = new TreeNode(val);
+
+            if (val < root.val && root.left != null) InsertIntoBST(root.left, val);
+            else if (val < root.val) root.left = new TreeNode(val);
+
+            return root;
+        }
+    }
+}
