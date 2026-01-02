37b499ff0d7993f01280013ae40a2730cb016cf1
Jason <j-folkens@hotmail.com>
2026-01-02 09:37:50 -0500

    Add iterative stack-based solution for Same Tree (100)

---
+++ LeetCodeProblems/Tree/SameTree_Stack_CSharp_100.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Interfaces.Easy;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    // solved 2025-12-24
+    public class SameTree_Stack_CSharp_100 : ISameTree_100
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            var stack = new Stack<(TreeNode? pNode, TreeNode? qNode)>();
+            stack.Push((p, q));
+
+            while (stack.Count > 0)
+            {
+                (var pNode, var qNode) = stack.Pop();
+                if (pNode == null && qNode == null) continue;
+                if (pNode?.val != qNode?.val) return false;
+
+                stack.Push((pNode?.left, qNode?.left));
+                stack.Push((pNode?.right, qNode?.right));
+            }
+            return true;
+        }
+    }
+}
