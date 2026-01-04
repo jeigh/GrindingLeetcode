335777ecefcfd4e04f03f040b1e434c770f203b1
Jason <j-folkens@hotmail.com>
2026-01-04 09:47:21 -0500

    Add ITwoSumIV_653 interface for Two Sum IV problem

---
+++ LeetCodeProblems.Interfaces/Easy/ITwoSumIV_653.cs
@@ -0,0 +1,9 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Easy
+{
+    public interface ITwoSumIV_653
+    {
+        public bool FindTarget(TreeNode root, int k);
+    }
+}
