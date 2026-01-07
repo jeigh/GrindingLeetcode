2fab7d33c60be2147eeca192926e755c537846fa
Jason <j-folkens@hotmail.com>
2026-01-07 10:22:16 -0500

    interface for 230

---
+++ LeetCodeProblems.Interfaces/Medium/IKthSmallestElementInBST_230.cs
@@ -0,0 +1,9 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface IKthSmallestElementInBST_230
+    {
+        public int KthSmallest(TreeNode root, int k);
+    }
+}
