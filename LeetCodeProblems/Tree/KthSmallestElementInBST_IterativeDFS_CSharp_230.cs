b8ccdcffe12f028fdb4df637f13ca743df06e949
Jason <j-folkens@hotmail.com>
2026-01-07 10:23:21 -0500

    230 iterative

---
+++ LeetCodeProblems/Tree/KthSmallestElementInBST_IterativeDFS_CSharp_230.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class KthSmallestElementInBST_IterativeDFS_CSharp_230 : IKthSmallestElementInBST_230
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            if (root == null || k == 0) return 0;
+            var stack = new Stack<TreeNode>();
+            var current = root;
+
+            while (k > 0 && (current != null || stack.Count > 0))
+            {
+                while (current != null)
+                {
+                    stack.Push(current);
+                    current = current.left;
+                }
+
+                current = stack.Pop();
+                k--;
+                if (k > 0) current = current.right;
+            }
+
+            if (current != null) return current.val;
+            return 0; 
+        }
+    }
+}
