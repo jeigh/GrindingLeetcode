3c868ac6c645825f7ecc57e6440bbfb3cfbfa8bf
Jason <j-folkens@hotmail.com>
2026-01-08 16:56:16 -0500

    LC 230 Recursive

---
+++ LeetCodeProblems/Tree/KthSmallestElementInBST_RecursiveDFS_CSharp_230.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class KthSmallestElementInBST_RecursiveDFS_CSharp_230 : IKthSmallestElementInBST_230
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            return InOrderTraversal(root, ref k).Value;
+        }
+
+        public int? InOrderTraversal(TreeNode root, ref int k)
+        {
+            if (root == null) return null;
+            var left = InOrderTraversal(root.left, ref k);
+            if (left.HasValue) return left;
+            if (--k == 0) return root.val;
+            return InOrderTraversal(root.right, ref k);
+        }
+    }
+}
