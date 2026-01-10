37fe48ea88095d18b2b2177a9034fe34df40b5a3
Jason <j-folkens@hotmail.com>
2026-01-10 11:35:26 -0500

    interface for lc 450

---
+++ LeetCodeProblems.Interfaces/Medium/IDeleteNodeInABST_450.cs
@@ -0,0 +1,9 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface IDeleteNodeInABST_450
+    {
+        public TreeNode DeleteNode(TreeNode root, int key);
+    }
+}
