098d5886d1218177a7c7fb0507da98c79206d2ed
Jason <j-folkens@hotmail.com>
2026-01-13 08:25:01 -0500

    interface for LC 102

---
+++ LeetCodeProblems.Interfaces/Medium/IBinaryTreeLevelOrderTraversal_102.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface IBinaryTreeLevelOrderTraversal_102
+    {
+        public List<List<int>> LevelOrder(TreeNode root);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
