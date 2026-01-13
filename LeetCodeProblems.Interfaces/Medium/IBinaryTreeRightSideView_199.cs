6402ccdb9a8ff367825caaba7d7919b3a48df839
Jason <j-folkens@hotmail.com>
2026-01-13 08:25:19 -0500

    interface for LC 199

---
+++ LeetCodeProblems.Interfaces/Medium/IBinaryTreeRightSideView_199.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface IBinaryTreeRightSideView_199
+    {
+        public List<int> RightSideView(TreeNode root);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
