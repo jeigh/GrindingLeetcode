7ecdb455f92f9d9aacff55c893f852fd05e1584a
Jason <j-folkens@hotmail.com>
2026-01-20 09:36:57 -0500

    LC 450 LeftTreeDemotion

---
+++ LeetCodeProblems/Tree/DeleteNodeInABST_LeftTreeDemotion_CSharp_450.cs
@@ -0,0 +1,42 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class DeleteNodeInABST_LeftTreeDemotion_CSharp_450 : IDeleteNodeInABST_450
+    {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null) return null;
+
+            if (key > root.val)
+            {
+                root.right = DeleteNode(root.right, key);
+                return root;
+            }
+            if (key < root.val)
+            {
+                root.left = DeleteNode(root.left, key);
+                return root;
+            }
+
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+
+            var leftSubtree = root.left;
+            var promotedNode = root.right;
+
+            DemoteLeftSubtree(root, leftSubtree);
+
+            return promotedNode;
+        }
+
+        private void DemoteLeftSubtree(TreeNode deletedNode, TreeNode leftSubtree)
+        {
+            var attachPoint = deletedNode.right;
+            while (attachPoint.left != null) attachPoint = attachPoint.left;
+
+            attachPoint.left = leftSubtree;  
+        }
+    }
+}
