c4f56707ad36f4af1b91386a28e0bd1cdeb96e41
Jason <j-folkens@hotmail.com>
2026-01-22 10:16:59 -0500

    LC 99, Recursive VB

---
+++ LeetCodeProblems.VisualBasic/Tree/RecoverBinarySearchTree_Recursive_VB_99.vb
@@ -0,0 +1,44 @@
+﻿Imports LeetCodeProblems.Interfaces.Medium
+Imports LeetCodeProblems.Shared
+
+' solved 2026-01-19
+Public Class RecoverBinarySearchTree_Recursive_VB_99
+    Implements IRecoverBinarySearchTree_99
+
+    Public Sub RecoverTree(root As TreeNode) Implements IRecoverBinarySearchTree_99.RecoverTree
+        If root Is Nothing Then Return
+
+        Dim first As TreeNode = Nothing
+        Dim last As TreeNode = Nothing
+        Dim previous As TreeNode = Nothing
+
+        InOrder(root, first, last, previous)
+
+        Swap(first, last)
+    End Sub
+
+    Public Sub InOrder(node As TreeNode, ByRef first As TreeNode, ByRef last As TreeNode, ByRef previous As TreeNode)
+        If node Is Nothing Then Return
+        InOrder(node.left, first, last, previous)
+
+        If previous IsNot Nothing AndAlso previous.val > node.val Then
+            If first Is Nothing Then
+                first = previous
+                last = node
+            Else
+                last = node
+            End If
+        End If
+        previous = node
+
+        InOrder(node.right, first, last, previous)
+    End Sub
+
+    Public Sub Swap(first As TreeNode, last As TreeNode)
+        Dim temp = first.val
+        first.val = last.val
+        last.val = temp
+    End Sub
+
+End Class
+
