a1342d906174ef8787b45df45ca30880899594f2
Jason <j-folkens@hotmail.com>
2026-01-22 10:17:54 -0500

    LC 99 recursive C#

---
+++ LeetCodeProblems/Tree/RecoverBinarySearchTree_Recursive_CSharp_99.cs
@@ -0,0 +1,59 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    // solved 2026-01-17
+    public class RecoverBinarySearchTree_Recursive_CSharp_99 : IRecoverBinarySearchTree_99
+    {
+        private TreeNode _first;
+        private TreeNode _last;
+        private TreeNode _previous;
+
+        public void RecoverTree(TreeNode root)
+        {
+            _first = null;
+            _last = null;
+            _previous = null;
+        
+
+            InOrderTraversal(root);
+            Swap();
+        }
+
+        private void Swap()
+        {
+            var temp = _first.val;
+            _first.val = _last.val;
+            _last.val = temp;
+        }
+
+        private void InOrderTraversal(TreeNode node)
+        {
+            if (node == null) return;
+
+            InOrderTraversal(node.left);
+
+            if (_previous != null &&  _previous.val > node.val)
+            {
+                if (_first == null)
+                {
+                    _first = _previous;
+                    _last = node;
+
+                }
+                else
+                {
+                    _last = node;
+                }
+            }
+
+            _previous = node;
+
+            InOrderTraversal(node.right);
+        }
+
+
+
+    }
+}
