50382035ea123d6deee08a087ae281bd13f03723
Jason <j-folkens@hotmail.com>
2026-01-24 12:58:56 -0500

    interface for lc 1448

---
+++ LeetCodeProblems.Interfaces/Medium/ICountGoodNodesInBinaryTree_1448.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface ICountGoodNodesInBinaryTree_1448
+    {
+        public int GoodNodes(TreeNode root);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
