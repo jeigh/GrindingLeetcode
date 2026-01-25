79d20e07139a56395d9fd4cfbad403fd80e218a7
Jason <j-folkens@hotmail.com>
2026-01-25 10:59:54 -0500

    this interface was referenced by a previous commit

---
+++ LeetCodeProblems.Interfaces/Medium/IValidateBinarySearchTree_98.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface IValidateBinarySearchTree_98
+    {
+        public bool IsValidBST(TreeNode root);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
