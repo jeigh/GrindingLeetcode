b5527c7ffc09f971e2f3fdf43a1552d8dd0fa403
Jason <j-folkens@hotmail.com>
2026-01-25 10:58:04 -0500

    LC 98 InOrderTraversal

---
+++ LeetCodeProblems/Tree/ValidateBinarySearchTree_CSharp_InOrder_Recursive_98.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class ValidateBinarySearchTree_CSharp_InOrder_Recursive_98 : IValidateBinarySearchTree_98
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            if (root == null) return true;
+            var minValue = int.MinValue;
+            return recurse(root, ref minValue);
+        }
+
+        public bool recurse(TreeNode node, ref int prev)
+        {
+            if (node == null) return true;
+
+            if (!recurse(node.left, ref prev)) return false;
+
+            if (prev >= node.val) return false;
+            prev = node.val;
+
+            if (!recurse(node.right, ref prev)) return false;
+
+            return true;
+        }
+    }
+}
