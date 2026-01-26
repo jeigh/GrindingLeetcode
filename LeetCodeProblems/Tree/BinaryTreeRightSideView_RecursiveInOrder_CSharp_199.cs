16b2a7d6e1132521ecedf6ed60e8b30b6a0bbd30
Jason <j-folkens@hotmail.com>
2026-01-26 09:50:45 -0500

    LC 199 Recursive InOrder solution

---
+++ LeetCodeProblems/Tree/BinaryTreeRightSideView_RecursiveInOrder_CSharp_199.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class BinaryTreeRightSideView_RecursiveInOrder_CSharp_199 : IBinaryTreeRightSideView_199
+    {
+        public List<int> RightSideView(TreeNode root)
+        {
+            var returnable = new List<int>();
+            if (root == null) return returnable;
+            var depth = 0;
+            var responseCollection = new List<int>();
+            InOrder(root, depth, responseCollection);
+            return responseCollection;
+        }
+
+        private void InOrder(TreeNode node, int depth, List<int> responseCollection)
+        {
+            if (node == null) return;
+            InOrder(node.left, depth + 1, responseCollection);
+
+            while (responseCollection.Count < depth + 1) responseCollection.Add(node.val);
+            responseCollection[depth] = node.val;
+
+            InOrder(node.right, depth + 1, responseCollection);
+        }
+    }
+}
