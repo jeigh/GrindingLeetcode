4bfba1d3ee238d36d29f7fcf267a049ae45fced4
Jason <j-folkens@hotmail.com>
2026-01-27 10:25:33 -0500

    LC 199 Iterative InOrder

---
+++ LeetCodeProblems/Tree/BinaryTreeRightSideView_IterativeInOrder_CSharp_199.cs
@@ -0,0 +1,37 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    // solved 2026-01-18
+    public class BinaryTreeRightSideView_IterativeInOrder_CSharp_199 : IBinaryTreeRightSideView_199
+    {
+        public List<int> RightSideView(TreeNode root)
+        {
+            var returnable = new List<int>();
+            if (root == null) return returnable;
+            var stack = new Stack<(int depth, TreeNode node)>();
+            TreeNode node = root;
+            var depth = 0;
+            while (stack.Count > 0 || node != null)
+            {
+                while (node != null)
+                {
+                    stack.Push((depth, node));
+                    depth++;
+                    node = node.left;
+                }
+
+                (depth, node) = stack.Pop();
+
+                while (returnable.Count < depth + 1) returnable.Add(node.val);
+                returnable[depth] = node.val;
+
+                node = node.right;
+                depth++;
+            }
+
+            return returnable;
+        }
+    }
+}
