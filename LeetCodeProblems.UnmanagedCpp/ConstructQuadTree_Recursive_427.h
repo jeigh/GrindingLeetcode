bba956cfe8bffd8bbade20b0f839c8272b6261cd
Jason <j-folkens@hotmail.com>
2026-01-29 09:05:24 -0500

    Add recursive quad tree construction (LeetCode 427)
    Implemented ConstructQuadTree_Recursive_427 class in a new header file. This class provides a recursive approach to build a quad tree from a 2D grid, including methods for recursive subdivision and uniformity checking. Uses QuadNode for tree representation.

---
+++ LeetCodeProblems.UnmanagedCpp/ConstructQuadTree_Recursive_427.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "QuadNode.h"
+#include <vector>
+#include "ConstructQuadTree_427.h"
+
+
+class ConstructQuadTree_Recursive_427 : public ConstructQuadTree_427 {
+public:
+    inline QuadNode* construct(std::vector<std::vector<int>>& grid) { 
+        return recurse(grid, grid.size(), 0, 0);
+    }
+
+private:
+    inline QuadNode* recurse(std::vector<std::vector<int>>& grid, int regionSize, int rowStart, int colStart) 
+    {
+        if (isUniform(grid, regionSize, rowStart, colStart))
+            return new QuadNode(grid[rowStart][colStart] == 1, true);
+
+        int half = regionSize / 2;
+
+        auto topLeft = recurse(grid, half, rowStart, colStart);
+        auto topRight = recurse(grid, half, rowStart, colStart + half);
+        auto bottomLeft = recurse(grid, half, rowStart + half, colStart);
+        auto bottomRight = recurse(grid, half, rowStart + half, colStart + half);
+
+        return new QuadNode(false, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+
+    inline bool isUniform(std::vector<std::vector<int>>& grid, int regionSize, int rowStart, int colStart) 
+    {
+        auto challengeValue = grid[rowStart][colStart];
+
+        for (int i = 0; i < regionSize; i++) {
+            for (int j = 0; j < regionSize; j++) {
+                if (challengeValue != grid[rowStart + i][colStart + j])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+};
