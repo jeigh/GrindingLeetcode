7e488b93f23fdbb5f0475730b095e9e4da2de9f9
Jason <j-folkens@hotmail.com>
2026-02-01 11:31:21 -0500

    LC 427 C# Recursive-optimized

---
+++ LeetCodeProblems/Tree/ConstructQuadTree_Recursion_Optimized_CSharp_427.cs
@@ -0,0 +1,40 @@
+﻿using LeetCodeProblems.Interfaces;
+using LeetCodeProblems.Interfaces.Medium;
+
+namespace LeetCodeProblems.CSharp.Tree
+{
+    public class ConstructQuadTree_Recursion_Optimized_CSharp_427 : IConstructQuadTree_427
+    {
+        private readonly QuadNode falseLeaf = new QuadNode(false, true);
+        private readonly QuadNode trueLeaf = new QuadNode(true, true);
+
+        public QuadNode Construct(int[][] grid)
+        {
+            int n = grid.Length;
+            return Dfs(grid, n, 0, 0);
+        }
+
+        private QuadNode Dfs(int[][] grid, int regionSize, int rowStart, int columnStart)
+        {
+            if (regionSize == 1)
+                return grid[rowStart][columnStart] == 1 ? trueLeaf : falseLeaf;
+
+            int half = regionSize / 2;
+            var topLeft = Dfs(grid, half, rowStart, columnStart);
+            var topRight = Dfs(grid, half, rowStart, columnStart + half);
+            var bottomLeft = Dfs(grid, half, rowStart + half, columnStart);
+            var bottomRight = Dfs(grid, half, rowStart + half, columnStart + half);
+
+            if (topLeft.isLeaf && topRight.isLeaf &&
+                bottomLeft.isLeaf && bottomRight.isLeaf &&
+                topLeft.val == topRight.val &&
+                topLeft.val == bottomLeft.val &&
+                topLeft.val == bottomRight.val)
+            {
+                return topLeft;
+            }
+
+            return new QuadNode(false, false, topLeft, topRight, bottomLeft, bottomRight);
+        }
+    }
+}
