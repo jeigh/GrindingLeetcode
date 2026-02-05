09bd26dd871768c6572ce32e88dafc45d23624a2
Jason <j-folkens@hotmail.com>
2026-02-05 09:11:12 -0500

    interface for LC 105

---
+++ LeetCodeProblems.Interfaces/Medium/IConstructBinaryTreeFromPreorderAndInorderTraversal_105.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface IConstructBinaryTreeFromPreorderAndInorderTraversal_105
+    {
+        TreeNode BuildTree(int[] preorder, int[] inorder);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
