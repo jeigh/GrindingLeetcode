34008dafcaecf2ddbc8665c5bdf54b670117600d
Jason <j-folkens@hotmail.com>
2026-02-21 09:52:22 -0500

    interface for LC 889

---
+++ LeetCodeProblems.Interfaces/Medium/IConstructBinaryTreeFromPreorderAndPostorderTraversal_889.cs
@@ -0,0 +1,9 @@
+﻿using LeetCodeProblems.Shared;
+
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface IConstructBinaryTreeFromPreorderAndPostorderTraversal_889
+    {
+        public TreeNode ConstructFromPrePost(int[] preorder, int[] postorder);
+    }
+}
