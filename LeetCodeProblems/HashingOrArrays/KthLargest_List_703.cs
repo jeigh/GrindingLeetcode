5001224230d79037994c1cb0b8aaa5ac9d0907ba
Jason <j-folkens@hotmail.com>
2026-02-24 10:49:08 -0500

    updated 703

--- LeetCodeProblems/HashingOrArrays/KthLargest_List_703.cs
+++ LeetCodeProblems/HashingOrArrays/KthLargest_List_703.cs
@@ -1,6 +1,8 @@
-﻿namespace LeetCodeProblems
+﻿using LeetCodeProblems.Interfaces.Easy;
+
+namespace LeetCodeProblems.CSharp.HashingOrArrays
 {
-    public class KthLargestUsingList
+    public class KthLargest_List_703 : IKthLargestElementInAStream_703
     {
         // this was my first stab at this solution, with a slightly less ideal performance.
         // space: O(n), time: O(n^2)
@@ -8,9 +10,9 @@
         private List<int> items = new List<int>();
         private readonly int _k;
 
-        public KthLargestUsingList(int k, int[] nums)
+        public KthLargest_List_703(int k, int[] nums)
         {
-            this._k = k;
+            _k = k;
             foreach(var item in nums)
             {
                 Add(item);
