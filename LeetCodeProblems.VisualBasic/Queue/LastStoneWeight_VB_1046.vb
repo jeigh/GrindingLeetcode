ecc9afa28d6b64720867b26cfa869fc4d22cdb52
Jason <j-folkens@hotmail.com>
2026-02-26 15:32:17 -0500

    LC 1046 VB

---
+++ LeetCodeProblems.VisualBasic/Queue/LastStoneWeight_VB_1046.vb
@@ -0,0 +1,21 @@
+﻿Imports LeetCodeProblems.Interfaces.Easy
+
+Public Class LastStoneWeight_VB_1046
+    Implements ILastStoneWeight_1046
+
+    Public Function LastStoneWeight(stones() As Integer) As Integer Implements ILastStoneWeight_1046.LastStoneWeight
+        Dim queue As New PriorityQueue(Of Integer, Integer)(stones.Length)
+
+        For Each item In stones
+            queue.Enqueue(item, -item)
+        Next
+
+        While queue.Count > 1
+            Dim difference = queue.Dequeue - queue.Dequeue
+            If difference > 0 Then queue.Enqueue(difference, -difference)
+        End While
+
+        If queue.Count = 1 Then Return queue.Dequeue()
+        Return 0
+    End Function
+End Class
