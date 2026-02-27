910098c1b5a344832bd0c2711de36580db25c1e4
Jason <j-folkens@hotmail.com>
2026-02-27 13:15:02 -0500

    LC 337 VB

---
+++ LeetCodeProblems.VisualBasic/Tree/HouseRobberIII_VB_337.vb
@@ -0,0 +1,27 @@
+﻿Imports LeetCodeProblems.Interfaces.Medium
+Imports LeetCodeProblems.Shared
+
+Public Class HouseRobberIII_VB_337
+    Implements IHouseRobberIII_337
+
+    Public Function Rob(root As TreeNode) As Integer Implements IHouseRobberIII_337.Rob
+        Dim results = RobSubTree(root)
+
+        Return Math.Max(results.withRoot, results.withoutRoot)
+    End Function
+
+    Public Function RobSubTree(node As TreeNode) As (withRoot As Integer, withoutRoot As Integer)
+        If node Is Nothing Then Return (0, 0)
+
+        Dim left = RobSubTree(node.left)
+        Dim right = RobSubTree(node.right)
+
+        Dim withoutNode As Integer = Math.Max(left.withRoot, left.withoutRoot) + Math.Max(right.withRoot, right.withoutRoot)
+
+        Dim withNode As Integer = node.val + left.withoutRoot + right.withoutRoot
+
+        Return (withNode, withoutNode)
+    End Function
+
+End Class
+
