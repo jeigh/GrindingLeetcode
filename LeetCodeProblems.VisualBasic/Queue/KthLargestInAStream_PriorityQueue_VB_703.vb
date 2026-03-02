42911bb27ae704176b6650623def035a381d6f1e
Jason <j-folkens@hotmail.com>
2026-03-02 09:53:40 -0500

    LC 703

---
+++ LeetCodeProblems.VisualBasic/Queue/KthLargestInAStream_PriorityQueue_VB_703.vb
@@ -0,0 +1,27 @@
+﻿Imports LeetCodeProblems.Interfaces.Easy
+
+Public Class KthLargestInAStream_PriorityQueue_VB_703
+    Implements IKthLargestElementInAStream_703
+
+    Private _k As Integer
+    Private _queue As PriorityQueue(Of Integer, Integer)
+
+    Public Sub New(k As Integer, nums() As Integer)
+        _k = k
+        _queue = New PriorityQueue(Of Integer, Integer)()
+        For Each num As Integer In nums
+            Add(num)
+        Next
+    End Sub
+
+    Public Function Add(val As Integer) As Integer Implements IKthLargestElementInAStream_703.Add
+        If _queue.Count < _k OrElse val > _queue.Peek() Then
+            _queue.Enqueue(val, val)
+        End If
+        If _queue.Count > _k Then
+            _queue.Dequeue()
+        End If
+
+        Return _queue.Peek()
+    End Function
+End Class
