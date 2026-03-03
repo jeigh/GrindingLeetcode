57fb1429011e81f37a0b7c0218e3bf52ed3b58b0
Jason <j-folkens@hotmail.com>
2026-03-03 11:12:21 -0500

    LC 973

---
+++ LeetCodeProblems/Queue/KthClosestPointsToOrigin_CSharp_973.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+using System.Collections.Generic;
+
+namespace LeetCodeProblems.CSharp.Queue
+{
+    public class KthClosestPointsToOrigin_CSharp_973 : IKthClosestPointsToOrigin_973
+    {
+        public int[][] KClosest(int[][] points, int k)
+        {
+            var queue = new PriorityQueue<int[], double>();
+            foreach (var point in points)
+            {
+                var x = point[0];
+                var y = point[1];
+
+                var magnitude = Math.Sqrt(x * x + y * y);
+
+                queue.Enqueue(point, -magnitude);
+            }
+            while (queue.Count > k) queue.Dequeue();
+            return queue.UnorderedItems.Select(x => x.Element).ToArray();
+        }
+    }
+}
