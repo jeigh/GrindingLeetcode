c2e99dcab143edc3437c4164982221b71e62a546
Jason <j-folkens@hotmail.com>
2026-03-05 09:16:04 -0500

    interface for LC 355

---
+++ LeetCodeProblems.Interfaces/Medium/ITwitter_355.cs
@@ -0,0 +1,11 @@
+﻿
+namespace LeetCodeProblems.Interfaces.Medium
+{
+    public interface ITwitter_355
+    {
+        void Follow(int followerId, int followeeId);
+        IList<int> GetNewsFeed(int userId);
+        void PostTweet(int userId, int tweetId);
+        void Unfollow(int followerId, int followeeId);
+    }
+}
