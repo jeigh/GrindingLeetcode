2f812d9e7b49c2d3bbaea54466dc794c51b19976
Jason <j-folkens@hotmail.com>
2026-03-10 10:19:48 -0400

    LC 621 C# Greedy

---
+++ LeetCodeProblems/Queue/TaskScheduler_Greedy_CSharp_621.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.Interfaces.Medium;
+
+namespace LeetCodeProblems.CSharp.Queue
+{
+
+    public class TaskScheduler_Greedy_CSharp_621 : ITaskScheduler_621
+    {
+        public int LeastInterval(char[] tasks, int n)
+        {
+            int[] taskFrequencies = new int[26];
+            foreach (char task in tasks)
+            {
+                taskFrequencies[task - 'A']++;
+            }
+
+            Array.Sort(taskFrequencies);
+            int maxFrequency = taskFrequencies[25];
+            int idle = (maxFrequency - 1) * n;
+
+            for (int i = 24; i >= 0; i--)
+            {
+                idle -= Math.Min(maxFrequency - 1, taskFrequencies[i]);
+            }
+            return Math.Max(0, idle) + tasks.Length;
+        }
+    }
+}
