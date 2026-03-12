6fd80fe50ec988b5814ca005933b0b96ec31d192
Jason <j-folkens@hotmail.com>
2026-03-12 09:20:58 -0400

    LC 621 math vb

---
+++ LeetCodeProblems.VisualBasic/Queue/TaskScheduler_Math_VB_621.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Text
+Imports LeetCodeProblems.Interfaces.Medium
+
+Public Class TaskScheduler_Math_VB_621
+    Implements ITaskScheduler_621
+
+    Public Function LeastInterval(tasks() As Char, n As Integer) As Integer Implements ITaskScheduler_621.LeastInterval
+        Dim aggregates = GetAggregates(tasks)
+
+        Dim result As Integer = ((aggregates.maxCount - 1) * (n + 1)) + aggregates.groupSize
+
+        Return Math.Max(tasks.Count, result)
+    End Function
+
+    Public Function GetAggregates(tasks() As Char) As (maxCount As Integer, groupSize As Integer)
+        Dim hashmap = New Dictionary(Of Char, Integer)()
+
+        For Each task As Char In tasks
+            If hashmap.ContainsKey(task) Then
+                hashmap(task) += 1
+            Else
+                hashmap(task) = 1
+            End If
+        Next
+
+        Dim maxCount As Integer = 0
+        Dim groupSize As Integer = 0
+
+        For Each count As Integer In hashmap.Values
+            If count > maxCount Then
+                maxCount = count
+                groupSize = 1
+            ElseIf count = maxCount Then
+                groupSize += 1
+            End If
+        Next
+
+        Return (maxCount, groupSize)
+    End Function
+End Class
