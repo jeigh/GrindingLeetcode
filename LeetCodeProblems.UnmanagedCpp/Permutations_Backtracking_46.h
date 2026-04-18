d9332db8529742f3c1ef3ad8145046d54ebc3c12
Jason <j-folkens@hotmail.com>
2026-04-18 14:25:18 -0400

    permutations - backtracking cpp

--- LeetCodeProblems.UnmanagedCpp/Permutations_Backtracking_46.h
+++ LeetCodeProblems.UnmanagedCpp/Permutations_Backtracking_46.h
@@ -7,7 +7,32 @@
 // space complexity: O(n)
 class Permutations_Backtracking_46 : public AbstractPermutations_46 {
 public:
-    inline std::vector<std::vector<int>> permute(std::vector<int>& nums) override {
-        throw std::runtime_error("Not implemented");
+    inline std::vector<std::vector<int>> permute(std::vector<int>& nums) override 
+    {
+        std::vector<std::vector<int>> result;
+        std::vector<bool> pick(nums.size(), false);
+        std::vector<int> currentList;
+
+        backtrack(currentList, nums, pick, result);
+
+        return result;
+    }
+
+    inline void backtrack(std::vector<int>& currentList, std::vector<int>& nums, std::vector<bool>& pick, std::vector<std::vector<int>>& result) 
+    {
+        if (currentList.size() == nums.size()) {
+            result.push_back(currentList);
+            return;
+        }
+
+        for(int i = 0; i < nums.size(); i++) {
+            if (!pick[i]) {
+                currentList.push_back(nums[i]);
+                pick[i] = true;
+                backtrack(currentList, nums, pick, result);
+                currentList.pop_back();
+                pick[i] = false;
+            }
+        }
     }
 };
