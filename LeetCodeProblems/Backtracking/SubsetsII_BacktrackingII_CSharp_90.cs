25cb4207ea4a3f1be97780282364d7451d7c87af
Jason <j-folkens@hotmail.com>
2026-05-06 10:57:26 -0400

    this was leftover in my workspace.   I need to restart my backtracking curriculum after a few weeks of taking a break;  I'm committing this now expecting that I'll have another commit shortly that will remove some of the broken unit tests.

--- LeetCodeProblems/Backtracking/SubsetsII_BacktrackingII_CSharp_90.cs
+++ LeetCodeProblems/Backtracking/SubsetsII_BacktrackingII_CSharp_90.cs
@@ -2,7 +2,7 @@ using LeetCodeProblems.Interfaces.Medium;
 
 namespace LeetCodeProblems.CSharp.Backtracking
 {
-    public class SubsetsII_BitMask_CSharp_90 : ISubsetsII_90
+    public class SubsetsII_BacktrackingII_CSharp_90 : ISubsetsII_90
     {
         // time complexity: O(n * 2^n)
         // space complexity: O(n * 2^n)
--- LeetCodeProblems/Backtracking/SubsetsII_Iterative_CSharp_90.cs
+++ LeetCodeProblems/Backtracking/SubsetsII_Iterative_CSharp_90.cs
@@ -8,7 +8,28 @@ namespace LeetCodeProblems.CSharp.Backtracking
         // space complexity: O(n * 2^n)
         public IList<IList<int>> SubsetsWithDup(int[] nums)
         {
-            throw new NotImplementedException();
+            var result = new List<IList<int>> {new List<int>()};
+            Array.Sort(nums);
+            int lastBatchSize = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool skipThisElement = i > 0 && nums[i] == nums[i-1];
+                int startIndex = skipThisElement ? result.Count - lastBatchSize : 0;
+                int newBatchSize = 0;
+                int endIndex = result.Count;
+                for (int j = startIndex; j < endIndex; j++)
+                {
+                    var copy = result[j].ToList();
+                    copy.Add(nums[i]);
+                    result.Add(copy);
+
+                    newBatchSize++;
+                }
+                lastBatchSize = newBatchSize;
+            }
+
+            return result;
         }
     }
 }
